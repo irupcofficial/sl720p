c8fb85730afc345bb20f40b8671c9a7fddc1dcec
Irupc <68991705+Irupc@users.noreply.github.com>
2020-10-26 15:06:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,6 @@
     </style>
 
     </head>
-    
     <body data-rsssl=1 class="home blog elementor-default">
         <div id="dt_contenedor">
             <header id="header" class="main">
@@ -64,39 +63,52 @@
                                 <!--Buttons in header-->
                                 <li><a href="https://www.irupc.net" aria-current="page">Home</a></li>
                                 <li><a><i class="fas fa-caret-down"></i>Movies</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="https://www.irupc.net/search/label/Adventure">Hollywood</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Action">Bollywood</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Comedy">Kollywood</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Family">සිංහල</a></li>
-                                    </ul>
+                                    <ul class="sub-menu"><div class="irupcli">
+                                        ---------------------------------------------------<br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcenglish()">English</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupchindi()">Hindi</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupctamil()">Tamil</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupctelugu()">Telugu</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcmalayalam()">Malayalam</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupckannada()">Kannada</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcsinhala()">Tamil</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcother()">Other</m><br></div><br><br>
+                                        ---------------------------------------------------<br><br>
+                                    </div></ul>
                                 </li>
-                                <li><a href="https://irupc.net/search/label/TV">TV Shows</a></li>
+                                <li><a onclick="irupctv()">TV Shows</a></li>
                                 <li><a><i class="fas fa-caret-down"></i>Genres</a>
                                     <ul class="sub-menu">
-                                        <li><a href="https://www.irupc.net/search/label/Adventure">Adventure</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Action">Action</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Comedy">Comedy</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Family">Family</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Horror/">Horror</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Fantasy">Fantasy</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Thriller">Thriller</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Animation">Animation</a></li>
-                                        <li><a href="https://www.irupc.net/search/label/Crime">Crime</a></li>
+                                        ---------------------------------------------------<br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcaction()">Action</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcadventure()">Adventure</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcromance()">Romance</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupccrime()">Crime</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupccomedy()">Comedy</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcdrama()">Drama</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupchorror()">Horror</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcaction()">Family</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcadventure()">Fantasy</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcromance()">Animation</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcdocumentary()">Documentary</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcthriller()">Thriller</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcsci()">Sci-Fi</m><br></div><br><br>
+                                        <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupchistory()">History</m><br></div><br><br>
+                                        ---------------------------------------------------<br><br>
                                     </ul>
                                 </li>
-                                <li><a href="https://www.irupc.net/p/contact-us.html">Contact US</a></li>
-                                <li><a href="https://forms.gle/w1mu2FCgwzsZyJ8m7"><b>Request Movies</b></a></li>
+                                <li><a onclick="irupccontact()">Contact US</a></li>
+                                <li><a onclick="irupcrequest()"><b>Request Movies</b></a></li>
                             </ul>
                         </div>
                     </div>
                             
                     <div class="headitems register_active">
                         <div id="advc-menu" class="search">
-                            <form action="https://www.irupc.net/search" method="GET"> 
-                                <input class="search-input" type="text" name="q" placeholder="Search..." value="" spellcheck="false"> 
-                                <button type="submit" id=""><span class="icon-search2"></span><i class="fab fa-searchengin"></i></button>
-                            </form>
+                        <form action="https://www.irupc.net/search" method="GET"> 
+                            <input class="search-input" type="text" name="q" placeholder="Search..." value="" spellcheck="false"> 
+                            <button type="submit" id=""><span class="icon-search2"></span><i class="fab fa-searchengin"></i></button>
+                        </form>
                         </div>
                     <div class="dtuser">
                         <a href="#" class="clicklogin"><i class="far fa-smile-wink"></i></a>
@@ -107,8 +119,8 @@
         </header>
         <div class="fixheadresp">
             <header class="responsive">
-                <div class="nav"><a class="aresp nav-resp"></a></div>
-                <div class="search"><a class="aresp search-resp"><i class="fas fa-search"></i></a></div>
+                <div class="ipnav"><a class="aresp nav-resp"><i class="fas fa-bars"></i></a></div>
+                <div class="ipsearch"><a class="aresp search-resp"><i class="fas fa-search"></i></a></div>
                 <div class="logo"> <a href="https://www.irupc.net/"><h1 class='text'>IruPC.net</h1></a> </div>
             </header>
             <div class="search_responsive">
@@ -127,28 +139,37 @@
                             <li><a href="https://www.irupc.net" aria-current="page">Home</a></li>
                             <li><a>Movies</a>
                                 <ul class="sub-menu">
-                                    <li><a href="https://www.irupc.net/search/label/Adventure">Hollywood</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Action">Bollywood</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Comedy">Kollywood</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Family">සිංහල</a></li>
+                                    <li><a onclick="irupcenglish()">English</a></li>
+                                    <li><a onclick="irupchindi()">Hindi</a></li>
+                                    <li><a onclick="irupctamil()">Tamil</a></li>
+                                    <li><a onclick="irupctelugu()">Telugu</a></li>
+                                    <li><a onclick="irupcmalayalam()">Malayalam</a></li>
+                                    <li><a onclick="irupckannada()">Kannada</a></li>
+                                    <li><a onclick="irupcsinhala()">Tamil</a></li>
+                                    <li><a onclick="irupcother()">Other</a></li>
                                 </ul>
                             </li>
                             <li><a href="https://www.irupc.net/search/label/TV">TV Shows</a></li>
                             <li><a>Genres</a>
                                 <ul class="sub-menu">
-                                    <li><a href="https://www.irupc.net/search/label/Adventure">Adventure</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Action">Action</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Comedy">Comedy</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Family">Family</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Horror/">Horror</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Fantasy">Fantasy</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Thriller">Thriller</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Animation">Animation</a></li>
-                                    <li><a href="https://www.irupc.net/search/label/Crime">Crime</a></li>
+                                    <li><a onclick="irupcaction()">Action</a></li>
+                                    <li><a onclick="irupcadventure()">Adventure</a></li>
+                                    <li><a onclick="irupcromance()">Romance</a></li>
+                                    <li><a onclick="irupccrime()">Crime</a></li>
+                                    <li><a onclick="irupccomedy()">Comedy</a></li>
+                                    <li><a onclick="irupcdrama()">Drama</a></li>
+                                    <li><a onclick="irupchorror()">Horror</a></li>
+                                    <li><a onclick="irupcaction()">Family</a></li>
+                                    <li><a onclick="irupcadventure()">Fantasy</a></li>
+                                    <li><a onclick="irupcromance()">Animation</a></li>
+                                    <li><a onclick="irupcdocumentary()">Documentary</a></li>
+                                    <li><a onclick="irupcthriller()">Thriller</a></li>
+                                    <li><a onclick="irupcsci()">Sci-Fi</a></li>
+                                    <li><a onclick="irupchistory()">History</a></li>
                                 </ul>
                             </li>
-                            <li><a href="https://www.irupc.net/p/contact-us.html">Contact US</a></li>
-                            <li><a href="https://forms.gle/w1mu2FCgwzsZyJ8m7"><b>Request Movies</b></a></li>
+                            <li><a onclick="irupccontact()">Contact US</a></li>
+                            <li><a onclick="irupcrequest()"><b>Request Movies</b></a></li>
                         </ul>
                     </div>
                 </div>
@@ -159,33 +180,33 @@
                 <div class="fixresp">
                     <!--Letters-->
                     <ul class="glossary">
-                        <li><a href="https://www.irupc.net/search/label/#" target="_blank">#</a></li>
-                        <li><a href="https://www.irupc.net/search/label/A" target="_blank">A</a></li>
-                        <li><a href="https://www.irupc.net/search/label/B" target="_blank">B</a></li>
-                        <li><a href="https://www.irupc.net/search/label/C" target="_blank">C</a></li>
-                        <li><a href="https://www.irupc.net/search/label/D" target="_blank">D</a></li>
-                        <li><a href="https://www.irupc.net/search/label/E" target="_blank">E</a></li>
-                        <li><a href="https://www.irupc.net/search/label/F" target="_blank">F</a></li>
-                        <li><a href="https://www.irupc.net/search/label/G" target="_blank">G</a></li>
-                        <li><a href="https://www.irupc.net/search/label/H" target="_blank">H</a></li>
-                        <li><a href="https://www.irupc.net/search/label/I" target="_blank">I</a></li>
-                        <li><a href="https://www.irupc.net/search/label/J" target="_blank">J</a></li>
-                        <li><a href="https://www.irupc.net/search/label/K" target="_blank">K</a></li>
-                        <li><a href="https://www.irupc.net/search/label/L" target="_blank">L</a></li>
-                        <li><a href="https://www.irupc.net/search/label/M" target="_blank">M</a></li>
-                        <li><a href="https://www.irupc.net/search/label/N" target="_blank">N</a></li>
-                        <li><a href="https://www.irupc.net/search/label/O" target="_blank">O</a></li>
-                        <li><a href="https://www.irupc.net/search/label/P" target="_blank">P</a></li>
-                        <li><a href="https://www.irupc.net/search/label/Q" target="_blank">Q</a></li>
-                        <li><a href="https://www.irupc.net/search/label/R" target="_blank">R</a></li>
-                        <li><a href="https://www.irupc.net/search/label/S" target="_blank">S</a></li>
-                        <li><a href="https://www.irupc.net/search/label/T" target="_blank">T</a></li>
-                        <li><a href="https://www.irupc.net/search/label/U" target="_blank">U</a></li>
-                        <li><a href="https://www.irupc.net/search/label/V" target="_blank">V</a></li>
-                        <li><a href="https://www.irupc.net/search/label/W" target="_blank">W</a></li>
-                        <li><a href="https://www.irupc.net/search/label/X" target="_blank">X</a></li>
-                        <li><a href="https://www.irupc.net/search/label/Y" target="_blank">Y</a></li>
-                        <li><a href="https://www.irupc.net/search/label/Z" target="_blank">Z</a></li>
+                        <li><a onclick="letterdigit()" target="_blank">#</a></li>
+                        <li><a onclick="letterA()" target="_blank">A</a></li>
+                        <li><a onclick="letterB()" target="_blank">B</a></li>
+                        <li><a onclick="letterC()" target="_blank">C</a></li>
+                        <li><a onclick="letterD()" target="_blank">D</a></li>
+                        <li><a onclick="letterE()" target="_blank">E</a></li>
+                        <li><a onclick="letterF()" target="_blank">F</a></li>
+                        <li><a onclick="letterG()" target="_blank">G</a></li>
+                        <li><a onclick="letterH()" target="_blank">H</a></li>
+                        <li><a onclick="letterI()" target="_blank">I</a></li>
+                        <li><a onclick="letterJ()" target="_blank">J</a></li>
+                        <li><a onclick="letterK()" target="_blank">K</a></li>
+                        <li><a onclick="letterL()" target="_blank">L</a></li>
+                        <li><a onclick="letterM()" target="_blank">M</a></li>
+                        <li><a onclick="letterN()" target="_blank">N</a></li>
+                        <li><a onclick="letterO()" target="_blank">O</a></li>
+                        <li><a onclick="letterP()" target="_blank">P</a></li>
+                        <li><a onclick="letterQ()" target="_blank">Q</a></li>
+                        <li><a onclick="letterR()" target="_blank">R</a></li>
+                        <li><a onclick="letterS()" target="_blank">S</a></li>
+                        <li><a onclick="letterT()" target="_blank">T</a></li>
+                        <li><a onclick="letterU()" target="_blank">U</a></li>
+                        <li><a onclick="letterV()" target="_blank">V</a></li>
+                        <li><a onclick="letterW()" target="_blank">W</a></li>
+                        <li><a onclick="letterX()" target="_blank">X</a></li>
+                        <li><a onclick="letterY()" target="_blank">Y</a></li>
+                        <li><a onclick="letterZ()" target="_blank">Z</a></li>
                     </ul>
                 </div>
                 <div class="items_glossary"></div>
