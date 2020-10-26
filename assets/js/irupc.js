a6128241c5b609eba1ecde7fdf14eb215e0e1289
Irupc <68991705+Irupc@users.noreply.github.com>
2020-10-26 06:35:07 +0530

    Create irupc.js

---
+++ assets/js/irupc.js
@@ -0,0 +1,18 @@
+       function ipyear1(){
+        document.getElementById('ipyear1').style.display='block'; document.getElementById('ipyear2').style.display='none';
+    }
+    function ipyear2(){
+        document.getElementById('ipyear2').style.display='block'; document.getElementById('ipyear1').style.display='none';
+    }
+    function ipgenre1(){
+        document.getElementById('ipgenre1').style.display='block'; document.getElementById('ipgenre2').style.display='none';
+    }
+    function ipgenre2(){
+        document.getElementById('ipgenre2').style.display='block'; document.getElementById('ipgenre1').style.display='none';
+    }
+    function iplang1(){
+        document.getElementById('iplang1').style.display='block'; document.getElementById('iplang2').style.display='none';
+    }
+    function iplang2(){
+        document.getElementById('iplang2').style.display='block'; document.getElementById('iplang1').style.display='none';
+    }
