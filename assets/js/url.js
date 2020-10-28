3a2548e2b194e9fb8a7e7adc38141a0e90f563f6
Irupc <68991705+Irupc@users.noreply.github.com>
2020-10-28 22:53:27 +0530

    Update url.js

--- assets/js/url.js
+++ assets/js/url.js
@@ -1,3 +1,26 @@
-        function irupc2020(){
-            window.open('https://google.com')
+        function irupcMoviepgurl(pgno){
+            window.open("https://movieslk.herokuapp.com/Movies/"+pgno+".php")
         }
+
+        function irupcGenrepgurl(pgno, genre){
+            window.open("https://movieslk.herokuapp.com/Genres/"+genre+"/"+pgno+".php")
+        }
+
+        function irupcLangpgurl(pgno, lang){
+            window.open("https://movieslk.herokuapp.com/Langages/"+lang+"/"+pgno+".php")
+        }
+
+        function irupcgenre(iptype){
+            window.open("https://movieslk.herokuapp.com/Langages/"+iptype+"/1.php")
+        }
+        
+        function irupclang(iptype){
+            window.open("https://movieslk.herokuapp.com/Langages/"+iptype+"/1.php")
+        }
+
+        function irupcyear(iptype){
+            window.open("https://movieslk.herokuapp.com/Years/"+iptype+"/1.php")
+        }
+
+        totalmoviepgs=20;
+        ipNoOfMovies=370;
