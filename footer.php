69cd5a2f9506b5a7f12ae0bebda0892121e821fa
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 16:31:42 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+    <footer class="main">
+        <div class="fbox">
+            <div class="fcmpbox">
+                <div class="copy"><b>&copy; 2020 by <a href="https://www.irupc.net/">IruPC</strong></a>. All Rights Reserved.</b></div>
+                <span class="top-page">
+                    <a id="top-page"><i class="fas fa-angle-double-up"></i></a>
+                </span>
+                <div class="fmenu">
+                    <ul id="menu-footer" class="menu">
+                        <li id="menu-item-338" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-338">
+                            <a href="https://www.irupc.net/p/contact-us.html">Contact Us</a>
+                        </li>
+                        <li id="menu-item-365" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-365">
+                            <a href="https://forms.gle/w1mu2FCgwzsZyJ8m7">Request Movies</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
