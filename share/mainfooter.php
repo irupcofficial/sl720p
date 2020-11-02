c237934bcaad4764835bdfe838e3960446673cbd
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 17:18:12 +0530

    Create mainfooter.php

---
+++ share/mainfooter.php
@@ -0,0 +1,17 @@
+<footer class="main">
+    <div class="fbox">
+        <div class="fcmpbox">
+            <div class="copy"><b>&copy; 2020 by <a href="https://www.irupc.net/">IruPC</strong></a>. All Rights Reserved.</b></div>
+            <span class="top-page">
+                <a id="top-page"><i class="fas fa-angle-double-up"></i></a>
+            </span>
+            <div class="fmenu">
+                <ul id="menu-footer" class="menu">
+                    <li><a onclick="ipMovieCollections()">Movie Collections</a></li>
+                    <li><a onclick="irupccontact()">Contact Us</a></li>
+                    <li><a onclick="irupcrequest()">Request Movies</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
