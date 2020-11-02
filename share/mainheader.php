5164aa7945244e1818daf32cf288385fe9d7ac47
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 17:16:45 +0530

    Create mainheader.php

---
+++ share/mainheader.php
@@ -0,0 +1,64 @@
+<header id="header" class="main">
+    <div class="hbox">
+        <div class="logo"><a href="https://www.irupc.net"><h1 class='text'>IruPC.net</h1></a></div>
+        <div class="head-main-nav">
+            <div class="menu-primary-menu-container">
+                <ul id="main_header" class="main-header">
+                    <!--Buttons in header-->
+                    <li><a href="https://www.irupc.net" aria-current="page">Home</a></li>
+                    <li><a onclick="irupcMoviepgurl(1)"><i class="fas fa-caret-down"></i>Movies</a>
+                        <ul class="sub-menu"><div class="irupcli">
+                            ---------------------------------------------------<br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('English')">English</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Hindi')">Hindi</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Tamil')">Tamil</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Telugu')">Telugu</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Malayalam')">Malayalam</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Kannada')">Kannada</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Tamil')">Tamil</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Korean')">Korean</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Other')">Other</m><br></div><br><br>
+                            ---------------------------------------------------<br><br>
+                        </div></ul>
+                    </li>
+                    <li><a onclick="irupctvpgurl(1)">TV Shows</a></li>
+                    <li><a><i class="fas fa-caret-down"></i>Genres</a>
+                        <ul class="sub-menu">
+                            ---------------------------------------------------<br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Action')">Action</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Adventure')">Adventure</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Romance')">Romance</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Crime')">Crime</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Comedy')">Comedy</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Drama')">Drama</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Horror')">Horror</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Family')">Family</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Fantasy')">Fantasy</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Animation')">Animation</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Documentary')">Documentary</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Thriller')">Thriller</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Sci')">Sci-Fi</m><br></div><br><br>
+                            <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('History')">History</m><br></div><br><br>
+                            ---------------------------------------------------<br><br>
+                        </ul>
+                    </li>
+                    <li><a onclick="ipMovieCollections()">Movie Collections</a></li>
+                    <li><a onclick="irupccontact()">Contact US</a></li>
+                </ul>
+            </div>
+        </div>
+                
+        <div class="headitems register_active">
+            <div id="advc-menu" class="search">
+            <form action="https://www.irupc.net/search" method="GET"> 
+                <input class="search-input" type="text" name="q" placeholder="Search..." value="" spellcheck="false"> 
+                <button type="submit" id=""><span class="icon-search2"></span><i class="fab fa-searchengin"></i></button>
+            </form>
+            </div>
+        <div class="dtuser">
+            <a href="#" class="clicklogin"><i class="far fa-smile-wink"></i></a>
+        </div>
+    </div>
+    <div class="live-search ltr"></div>
+</div>
+</header>
