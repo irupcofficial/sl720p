a04df474a5e9aa8e4aa9b0294e307a210b1a483a
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 16:06:06 +0530

    Update 1.php

--- Genres/Animation/1.php
+++ Genres/Animation/1.php
@@ -230,7 +230,7 @@
             <div class="module">
                 <div class="content right">
                     <header class="archive_post">
-                        <h1>Animation</h1><span><script>document.write(totalAdventurepgs);</script> x 25</span>
+                        <h1>Animation</h1><span><script>document.write(totalAnimationpgs*25);</script></span>
                     </header>
                         <div id="archive-content" class="animation-2 items">
 
