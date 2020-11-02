c1e559e5090a136c6f94b0841614588b83f4d217
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 19:49:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -75,18 +75,18 @@
     });
     </script> 
 </head>
-    <body data-rsssl=1 class="home blog elementor-default">
-        <div id="dt_contenedor">
-            <div id="mainheader"></div>
-        <div class="fixheadresp">
-            <div id="respheader"></div>
-        </div>
-    <div id="contenedor">
-            <div id="letter"></div>
+<body data-rsssl=1 class="home blog elementor-default">
+    <div id="dt_contenedor">
+<div id="mainheader"></div>
+    <div class="fixheadresp">
+<div id="respheader"></div>
+    </div>
+<div id="contenedor">
+<div id="letter"></div>
             <div class="module">
                 <div class="content right">
                     <header class="archive_post">
-                        <h1>Action</h1><span><script>document.write(totalActionpgs*25);</script></span>
+                        <h1>Movies</h1><span><script>document.write(ipNoOfMovies);</script></span>
                     </header>
                         <div id="archive-content" class="animation-2 items">
 
@@ -542,27 +542,23 @@
                             <!-- 1 --> 
                         </div>
                         <div class="pagination">
-                            <span>Page <script>document.write(pgnumber);</script> of <script>document.write(totalActionpgs);</script></span>
+                            <span>Page <script>document.write(pgnumber);</script> of <script>document.write(totaloldpgs);</script></span>
                             <span class="current"><script>document.write(pgnumber);</script></span>
-                            <a onclick="ipgenrepgs('Action', 2)" class="inactive"><script>document.write(pgnumber+1);</script></a>
-                            <a onclick="ipgenrepgs('Action', 3)" class="inactive"><script>document.write(pgnumber+2);</script></a>
-                            <a onclick="ipgenrepgs('Action', 4)" class="inactive"><script>document.write(pgnumber+3);</script></a>
                         </div>
                         <div class='resppages'>
-                            <a onclick="ipgenrepgs('Action', 2)"><i class="fas fa-forward"></i> Next Page</a>
                         </div>
                     </div>
-                        </div>
-            <div id="nav"></div>
-            <div id="footer"></div>
- 
-    </div>
+<div id="nav"></div>
+            </div>        
     
-    <script type="text/javascript">
-        jQuery(document).ready(function($) {$("#featured-titles").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,4],itemsDesktopSmall:[980,4],itemsTablet:[768,3],itemsTabletSmall: false,itemsMobile:[479,2] });$(".nextf").click(function(){ $("#featured-titles").trigger("owl.next") });$(".prevf").click(function(){ $("#featured-titles").trigger("owl.prev") });$("#dt-movies").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall: false,itemsMobile:[479,3] });$("#dt-tvshows").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall:false,itemsMobile:[479,3] });$("#dt-episodes").owlCarousel({ autoPlay:3500,pagination:false,items:5,stopOnHover:true,itemsDesktop:[900,3],itemsDesktopSmall:[750,3],itemsTablet:[500,2],itemsMobile:[320,1] });$("#dt-seasons").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall:false,itemsMobile:[479,3] });$("#slider-movies").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$("#slider-tvshows").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$("#slider-movies-tvshows").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$(".reset").click(function(event){ if (!confirm( dtGonza.reset_all )) { event.preventDefault() } });$(".addcontent").click(function(event){ if(!confirm(dtGonza.manually_content)){ event.preventDefault() } });});(function(b,c,d,e,f,h,j){b.GoogleAnalyticsObject=f,b[f]=b[f]||function(){(b[f].q=b[f].q||[]).push(arguments)},b[f].l=1*new Date,h=c.createElement(d),j=c.getElementsByTagName(d)[0],h.async=1,h.src=e,j.parentNode.insertBefore(h,j)})(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-177017662-1","auto"),ga("send","pageview");
+<div id="footer"></div>
+        </div>
+        
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {$("#featured-titles").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,4],itemsDesktopSmall:[980,4],itemsTablet:[768,3],itemsTabletSmall: false,itemsMobile:[479,2] });$(".nextf").click(function(){ $("#featured-titles").trigger("owl.next") });$(".prevf").click(function(){ $("#featured-titles").trigger("owl.prev") });$("#dt-movies").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall: false,itemsMobile:[479,3] });$("#dt-tvshows").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall:false,itemsMobile:[479,3] });$("#dt-episodes").owlCarousel({ autoPlay:3500,pagination:false,items:5,stopOnHover:true,itemsDesktop:[900,3],itemsDesktopSmall:[750,3],itemsTablet:[500,2],itemsMobile:[320,1] });$("#dt-seasons").owlCarousel({ autoPlay:3500,items:7,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall:false,itemsMobile:[479,3] });$("#slider-movies").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$("#slider-tvshows").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$("#slider-movies-tvshows").owlCarousel({ autoPlay:4000,items:3,stopOnHover:true,pagination:true,itemsDesktop:[1199,2],itemsDesktopSmall:[980,2],itemsTablet:[768,2],itemsTabletSmall:[600,1],itemsMobile:[479,1] });$(".reset").click(function(event){ if (!confirm( dtGonza.reset_all )) { event.preventDefault() } });$(".addcontent").click(function(event){ if(!confirm(dtGonza.manually_content)){ event.preventDefault() } });});(function(b,c,d,e,f,h,j){b.GoogleAnalyticsObject=f,b[f]=b[f]||function(){(b[f].q=b[f].q||[]).push(arguments)},b[f].l=1*new Date,h=c.createElement(d),j=c.getElementsByTagName(d)[0],h.async=1,h.src=e,j.parentNode.insertBefore(h,j)})(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-177017662-1","auto"),ga("send","pageview");
+        </script>
+    <script type='text/javascript' src='https://movieslk.herokuapp.com/assets/js/front_scripts.js' id='scripts-js'>
     </script>
-<script type='text/javascript' src='https://movieslk.herokuapp.com/assets/js/front_scripts.js' id='scripts-js'>
-</script>
     <div id="oscuridad"></div>
-</body>
+    </body>
 </html>
