08f8030d61fe083035a0439f3af03d471393c6ce
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-02 23:36:23 +0530

    Add files via upload

---
+++ assets/Head/letter.php
@@ -0,0 +1,35 @@
+<div class="letter_home">
+    <div class="fixresp">
+        <!--Letters-->
+        <ul class="glossary">
+            <li><a onclick="irupcLetter('Digit')">#</a></li>
+            <li><a onclick="irupcLetter('A')">A</a></li>
+            <li><a onclick="irupcLetter('B')">B</a></li>
+            <li><a onclick="irupcLetter('C')">C</a></li>
+            <li><a onclick="irupcLetter('D')">D</a></li>
+            <li><a onclick="irupcLetter('E')">E</a></li>
+            <li><a onclick="irupcLetter('F')">F</a></li>
+            <li><a onclick="irupcLetter('G')">G</a></li>
+            <li><a onclick="irupcLetter('H')">H</a></li>
+            <li><a onclick="irupcLetter('I')">I</a></li>
+            <li><a onclick="irupcLetter('J')">J</a></li>
+            <li><a onclick="irupcLetter('K')">K</a></li>
+            <li><a onclick="irupcLetter('L')">L</a></li>
+            <li><a onclick="irupcLetter('M')">M</a></li>
+            <li><a onclick="irupcLetter('N')">N</a></li>
+            <li><a onclick="irupcLetter('O')">O</a></li>
+            <li><a onclick="irupcLetter('P')">P</a></li>
+            <li><a onclick="irupcLetter('Q')">Q</a></li>
+            <li><a onclick="irupcLetter('R')">R</a></li>
+            <li><a onclick="irupcLetter('S')">S</a></li>
+            <li><a onclick="irupcLetter('T')">T</a></li>
+            <li><a onclick="irupcLetter('U')">U</a></li>
+            <li><a onclick="irupcLetter('V')">V</a></li>
+            <li><a onclick="irupcLetter('W')">W</a></li>
+            <li><a onclick="irupcLetter('X')">X</a></li>
+            <li><a onclick="irupcLetter('Y')">Y</a></li>
+            <li><a onclick="irupcLetter('Z')">Z</a></li>
+        </ul>
+    </div>
+    <div class="items_glossary"></div>
+</div>
---
+++ assets/Head/nav.php
@@ -0,0 +1,164 @@
+<div class="sidebar right scrolling">
+    <div class="fixed-sidebar-blank">
+        <aside id="search-2" class="widget widget_search">
+            <form class="search-form" action="https://www.irupc.net/search" method="GET"> 
+                <input class="search-input" type="text" name="q" placeholder="Search..." value="" spellcheck="false"> 
+                <button type="submit" id=""><span class="icon-search2"></span></button>
+            </form>
+        </aside>
+        <aside class="irupcadds"><div style='text-align: center;'><script type="text/javascript" src="https://backend-ssp.adstudio.cloud/?c=b&m=s&placementId=2045"></script></div></aside>
+    <div class="dt_mainmeta">
+        <div class="ipnavsocial">
+            <div> 
+                <b class="irupcTelegram" onclick="iptelegram()"><span style="font-size: 20px;"><i class="fab fa-telegram-plane"></i></span>&ThickSpace; Join Our Telegram Channel</b>
+            </div>
+        </div>
+        <div id="ipgenre1">
+            <nav class="genres">
+                <br><div class="irupctext">Genres</div><br>
+                <ul class="genres scrolling">
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Action')" style="cursor: pointer;">Action</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Adventure')" style="cursor: pointer;">Adventure</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Romance')" style="cursor: pointer;">Romance</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Crime')" style="cursor: pointer;">Crime</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Comedy')" style="cursor: pointer;">Comedy</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Drama')" style="cursor: pointer;">Drama</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Horror')" style="cursor: pointer;">Horror</a></b><hr>
+                </ul>
+            </nav>
+            <P align="right" onclick="ipgenre2()" style="cursor:pointer;"><strong>Show More</strong></p>
+        </div>
+        <div id="ipgenre2">
+            <nav class="genres">
+                <br><div class="irupctext">Genres</div><br>
+                <ul class="genres scrolling">
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Family')" style="cursor: pointer;">Family</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Fantasy')" style="cursor: pointer;">Fantasy</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Animation')" style="cursor: pointer;">Animation</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Documentary')" style="cursor: pointer;">Documentary</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('Thriller')" style="cursor: pointer;">Thriller</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupcgenre('Sci')" style="cursor: pointer;">Sci-Fi</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupcgenre('History')" style="cursor: pointer;">History</b><hr>
+                </ul>
+            </nav>
+            <P align="right" onclick="ipgenre1()" style="cursor:pointer;"><strong>Go Back</strong></p>
+        </div><br><br>
+            <aside id="dtw_content-24" class="widget doothemes_widget">
+            <h2 class="widget-title"><b>Most Popular Movies</b></h2><br>
+            </aside>
+        <article class="w_item_a"> 
+            <a onclick="ipTopMovie1()">
+                <div class="image"> 
+                    <img src="http://movieslk.herokuapp.com/Uploads/1.jpg">
+                    <div class="data"><h3><script>document.write(ipTopMovie1);</script></h3></div>
+                </div> 
+            </a>
+        </article>
+        <article class="w_item_a"> 
+            <a onclick="ipTopMovie2()">
+                <div class="image"> 
+                    <img src="http://movieslk.herokuapp.com/Uploads/2.jpg">
+                    <div class="data"><h3><script>document.write(ipTopMovie2);</script></h3></div>
+                </div> 
+            </a>
+        </article>
+        <article class="w_item_a"> 
+            <a onclick="ipTopMovie2()">
+                <div class="image"> 
+                    <img src="http://movieslk.herokuapp.com/Uploads/3.jpg">
+                    <div class="data"><h3><script>document.write(ipTopMovie2);</script></h3></div>
+                </div> 
+            </a>
+        </article>
+        <div id="iplang1">
+            <nav class="genres">
+                <br><div class="irupctext">Languages</div><br>
+                <ul class="genres scrolling">
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupclang('English')" style="cursor: pointer;">English</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupclang('Hindi')" style="cursor: pointer;">Hindi</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupclang('Sinhala')" style="cursor: pointer;">Sinhala</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupclang('Tamil')" style="cursor: pointer;">Tamil</a></b><hr>
+                </ul>
+            </nav>
+            <P align="right" onclick="iplang2()" style="cursor:pointer;"><strong>Show More</strong></p>
+        </div>
+        <div id="iplang2">
+            <nav class="genres">
+                <br><div class="irupctext">Languages</div><br>
+                <ul class="genres scrolling">
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupclang('Telugu')" style="cursor: pointer;">Telugu</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupclang('Malayalam')" style="cursor: pointer;">Malayalam</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupclang('Kannada')" style="cursor: pointer;">Kannada</a></b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b onclick="irupclang('Korean')" style="cursor: pointer;">Korean</b><hr>
+                    &ThickSpace;&ThickSpace; <i class="far fa-dot-circle">&ThickSpace;&ThickSpace;&ThickSpace;</i><b><a onclick="irupclang('Other')" style="cursor: pointer;">Other</a></b><hr>
+                </ul>
+            </nav>
+            <P align="right" onclick="iplang1()" style="cursor:pointer;"><strong>Go Back</strong></p>
+        </div>
+    </div>
+    <div class="ipnavsocial">
+        <div> 
+            <b class="irupcWhatsApp" onclick="ipwhatsapp()"><span style="font-size: 20px;"><i class="fab fa-whatsapp"></i></span>&ThickSpace; Join Our WhatsApp Group!!!</b>
+        </div>
+    </div>
+    <div class="dt_mainmeta">
+        <div id="ipyear1">
+            <nav class="releases">
+                <br><div class="irupctext">Release year</div><br>
+                <ul class="releases scrolling">
+                    <li><a onclick="irupcyear(2020)">2020</a></li>
+                    <li><a onclick="irupcyear(2019)">2019</a></li>
+                    <li><a onclick="irupcyear(2018)">2018</a></li>
+                    <li><a onclick="irupcyear(2017)">2017</a></li>
+                    <li><a onclick="irupcyear(2015)">2015</a></li>
+                    <li><a onclick="irupcyear(2014)">2014</a></li>
+                    <li><a onclick="irupcyear(2013)">2013</a></li>
+                    <li><a onclick="irupcyear(2012)">2012</a></li>
+                    <li><a onclick="irupcyear(2011)">2011</a></li>
+                    <li><a onclick="irupcyear(2010)">2010</a></li>
+                </ul>
+            </nav>
+        <P align="right" onclick="ipyear2()" style="cursor:pointer;"><strong>Show More</strong></p>
+        </div>
+        <div id="ipyear2">
+            <nav class="releases">
+                <br><div class="irupctext">Release year</div><br>
+                <ul class="releases scrolling">
+                    <li><a onclick="irupcyear(2009)">2009</a></li>
+                    <li><a onclick="irupcyear(2008)">2008</a></li>
+                    <li><a onclick="irupcyear(2007)">2007</a></li>
+                    <li><a onclick="irupcyear(2006)">2006</a></li>
+                    <li><a onclick="irupcyear(2005)">2005</a></li>
+                    <li><a onclick="irupcyear(2004)">2004</a></li>
+                    <li><a onclick="irupcyear(2003)">2003</a></li>
+                    <li><a onclick="irupcyear(2002)">2002</a></li>
+                    <li><a onclick="irupcyear(2001)">2001</a></li>
+                    <li><a onclick="irupcyear(2000)">2000</a></li>
+                    <li><a onclick="irupcyear('old')">Old Movies</a></li>
+                </ul>
+            </nav>
+        <P align="right" onclick="ipyear1()" style="cursor:pointer;"><strong>Go Back</strong></p>
+        </div><br><br>
+            <aside id="dtw_content-24" class="widget doothemes_widget">
+            <h2 class="widget-title"><b>Most Popular TV Shows</b></h2>
+            </aside>
+            <article class="w_item_a"> 
+                <a onclick="ipTopTvShow1()">
+                    <div class="image"> 
+                        <img src="http://movieslk.herokuapp.com/Uploads/4.jpg">
+                        <div class="data"><script>document.write(ipTopTvShow1);</script></div>
+                    </div> 
+                </a>
+            </article>
+            <article class="w_item_a"> 
+                <a onclick="ipTopTvShow2()">
+                    <div class="image"> 
+                        <img src="http://movieslk.herokuapp.com/Uploads/5.jpg">
+                        <div class="data"><h3><script>document.write(ipTopTvShow2);</script></h3></div>
+                    </div> 
+                </a>
+            </article>
+                <b class="irupcFaceBook" onclick="ipfacebook()"><span style="font-size: 20px;"><i class="fab fa-facebook"></i></span>&ThickSpace; Visit Our Face Book Page</b>
+    </div>
+</div>
+</div>
