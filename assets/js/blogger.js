44b7c4d0730b97cc2b156480b72caa617ffb2421
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-09 21:44:05 +0530

    Create blogger.js

---
+++ assets/js/blogger.js
@@ -0,0 +1,14 @@
+var space="    ";
+  document.getElementById("ips1").innerHTML = space;
+  document.getElementById("ips2").innerHTML = space;
+  document.getElementById("ips3").innerHTML = space;
+  document.getElementById("ips4").innerHTML = space;
+    
+    document.getElementById("subtitle").innerHTML = "Watch Online";
+    
+function iruPcOver(){
+    document.getElementById("subtitle").innerHTML = "With සිංහල Subtitle";
+}
+function iruPcOut(){
+    document.getElementById("subtitle").innerHTML = "Watch Online";
+}
