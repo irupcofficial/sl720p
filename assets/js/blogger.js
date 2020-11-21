00c9756e8f7cef860310ccbfe92a0d2742d942c8
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-21 14:47:45 +0530

    Update blogger.js

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -4,30 +4,9 @@ var space="    ";
   document.getElementById("ips3").innerHTML = space;
   document.getElementById("ips4").innerHTML = space;
     
-    document.getElementById("subtitle").innerHTML = "Watch Online";
-    
-function iruPcOver(){
-    document.getElementById("subtitle").innerHTML = variableWordOver3;
-}
-function iruPcOut(){
-    document.getElementById("subtitle").innerHTML = variableWordOut3;
-}
-
-function iruPcOver1(){
-    document.getElementById("dlWC1").innerHTML = variableWordOver1;
-}
-function iruPcOut1(){
-    document.getElementById("dlWC1").innerHTML = variableWordOut1;
-}
-
-function iruPcOver2(){
-    document.getElementById("dlWC2").innerHTML = variableWordOver2;
-}
-function iruPcOut2(){
-    document.getElementById("dlWC2").innerHTML = variableWordOut2;
-}
-    document.getElementById("dlWC1").innerHTML = "Download Now";
-    document.getElementById("dlWC2").innerHTML = "Download Now";
+  document.getElementById("subtitle").innerHTML = "Watch Online";
+  document.getElementById("dlWC1").innerHTML = "Download Now";
+  document.getElementById("dlWC2").innerHTML = "Download Now";
 
 function ins(){ document.getElementById("numberOfSeason").innerHTML = " 00"; document.getElementById('ins').style.display='block'; document.getElementById('irupcS01').style.display='none'; document.getElementById('irupcS02').style.display='none'; document.getElementById('irupcS03').style.display='none'; document.getElementById('irupcS04').style.display='none'; document.getElementById('irupcS05').style.display='none'; document.getElementById('irupcS06').style.display='none';; document.getElementById('irupcS07').style.display='none'; document.getElementById('irupcS08').style.display='none'; document.getElementById('irupcS09').style.display='none'; document.getElementById('irupcS10').style.display='none'; document.getElementById('irupcS11').style.display='none'; document.getElementById('irupcS12').style.display='none'; document.getElementById('irupcS13').style.display='none'; document.getElementById('irupcS14').style.display='none'; document.getElementById('irupcS15').style.display='none'; }
 function season01(){ document.getElementById("numberOfSeason").innerHTML = " 01"; document.getElementById('irupcS01').style.display='block'; document.getElementById('irupcS02').style.display='none'; document.getElementById('irupcS03').style.display='none'; document.getElementById('ins').style.display='none'; document.getElementById('irupcS04').style.display='none'; document.getElementById('irupcS05').style.display='none'; document.getElementById('irupcS06').style.display='none'; document.getElementById('irupcS07').style.display='none'; document.getElementById('irupcS08').style.display='none'; document.getElementById('irupcS09').style.display='none'; document.getElementById('irupcS10').style.display='none'; document.getElementById('irupcS11').style.display='none'; document.getElementById('irupcS12').style.display='none'; document.getElementById('irupcS13').style.display='none'; document.getElementById('irupcS14').style.display='none'; document.getElementById('irupcS15').style.display='none'; }
