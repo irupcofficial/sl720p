a3a288ae06363daa9b23a55c165dc56651cd4452
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-22 08:58:42 +0530

    Create Dracula_4862.js

---
+++ assets/TV/Dracula_4862.js
@@ -0,0 +1,33 @@
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E01.HDTV.x264.mp4";
+s01e02x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E02.HDTV.x264.mp4";
+s01e03x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E03.HDTV.x264.mp4";
+s01e04x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E04.HDTV.x264.mp4";
+s01e05x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E05.HDTV.x264.mp4";
+s01e06x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E06.HDTV.x264.mp4";
+s01e07x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E07.HDTV.x264.mp4";
+s01e08x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E08.HDTV.x264.mp4";
+s01e09x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E09.HDTV.x264.mp4";
+s01e10x = "http://download.irupc.xyz/0:/TV.Shows/Dracula_2013/www.irupc.net__Dracula.2013.S01E10.HDTV.x264.mp4";
+
+s01e01y = "https://irupc-net.herokuapp.com/200493782309980/@linkslanka%20Dracula.2013.S01E01.HDTV.x264-LOL.mp4.mp4";
+s01e02y = "https://irupc-net.herokuapp.com/200510962179164/@linkslanka%20Dracula.2013.S01E02.HDTV.x264-LOL.mp4.mp4";
+s01e03y = "https://irupc-net.herokuapp.com/200528142048348/@linkslanka%20Dracula.2013.S01E03.HDTV.x264-LOL.mp4.mp4";
+s01e04y = "https://irupc-net.herokuapp.com/200545321917532/@linkslanka%20Dracula.2013.S01E04.HDTV.x264-LOL.mp4.mp4";
+s01e05y = "https://irupc-net.herokuapp.com/200562501786716/@linkslanka%20Dracula.2013.S01E05.HDTV.x264-LOL.mp4.mp4";
+s01e06y = "https://irupc-net.herokuapp.com/200579681655900/@linkslanka%20Dracula.2013.S01E06.HDTV.x264-LOL.mp4.mp4";
+s01e07y = "https://irupc-net.herokuapp.com/200596861525084/@linkslanka%20Dracula.2013.S01E07.HDTV.x264-LOL.mp4.mp4";
+s01e08y = "https://irupc-net.herokuapp.com/200614041394268/@linkslanka%20Dracula.2013.S01E08.HDTV.x264-LOL.mp4.mp4";
+s01e09y = "https://irupc-net.herokuapp.com/200631221263452/@linkslanka%20Dracula.2013.S01E09.HDTV.x264-LOL.mp4.mp4";
+s01e10y = "https://irupc-net.herokuapp.com/200648401132636/@linkslanka%20Dracula.2013.S01E10.HDTV.x264-LOL.mp4.mp4";
+
+s1Res = "SD"; 
+s1Size = "250MB";
+s1Rip = "HDTV";
+s1Msg1 = "x264 Codec File";
+s1Msg2 = "Warning! SD Quality Files";
+
+lastRes = "SD";
+lastSize = "251MB";
+lastRip = "HDTV";
+lastMsg1 = "x264 Codec File";
+lastMsg2 = "Warning! SD Quality";
