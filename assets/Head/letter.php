ba9e05689b484d94f2f70261a92749000725c758
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-25 19:24:15 +0530

    Update letter.php

--- assets/Head/letter.php
+++ assets/Head/letter.php
@@ -1,35 +1,35 @@
 <div class="letter_home">
-    <div class="fixresp">
-        <!--Letters-->
-        <ul class="glossary">
-            <li><a onclick="irupcLetter('Digit')">#</a></li>
-            <li><a onclick="irupcLetter('A')">A</a></li>
-            <li><a onclick="irupcLetter('B')">B</a></li>
-            <li><a onclick="irupcLetter('C')">C</a></li>
-            <li><a onclick="irupcLetter('D')">D</a></li>
-            <li><a onclick="irupcLetter('E')">E</a></li>
-            <li><a onclick="irupcLetter('F')">F</a></li>
-            <li><a onclick="irupcLetter('G')">G</a></li>
-            <li><a onclick="irupcLetter('H')">H</a></li>
-            <li><a onclick="irupcLetter('I')">I</a></li>
-            <li><a onclick="irupcLetter('J')">J</a></li>
-            <li><a onclick="irupcLetter('K')">K</a></li>
-            <li><a onclick="irupcLetter('L')">L</a></li>
-            <li><a onclick="irupcLetter('M')">M</a></li>
-            <li><a onclick="irupcLetter('N')">N</a></li>
-            <li><a onclick="irupcLetter('O')">O</a></li>
-            <li><a onclick="irupcLetter('P')">P</a></li>
-            <li><a onclick="irupcLetter('Q')">Q</a></li>
-            <li><a onclick="irupcLetter('R')">R</a></li>
-            <li><a onclick="irupcLetter('S')">S</a></li>
-            <li><a onclick="irupcLetter('T')">T</a></li>
-            <li><a onclick="irupcLetter('U')">U</a></li>
-            <li><a onclick="irupcLetter('V')">V</a></li>
-            <li><a onclick="irupcLetter('W')">W</a></li>
-            <li><a onclick="irupcLetter('X')">X</a></li>
-            <li><a onclick="irupcLetter('Y')">Y</a></li>
-            <li><a onclick="irupcLetter('Z')">Z</a></li>
-        </ul>
-    </div>
-    <div class="items_glossary"></div>
+  <div class="fixresp">
+      <!--Letters-->
+      <ul class="glossary">
+          <li><a href="/Letters/Digit/1.php">#</a></li>
+          <li><a href="/Letters/A/1.php">A</a></li>
+          <li><a href="/Letters/B/1.php">B</a></li>
+          <li><a href="/Letters/C/1.php">C</a></li>
+          <li><a href="/Letters/D/1.php">D</a></li>
+          <li><a href="/Letters/E/1.php">E</a></li>
+          <li><a href="/Letters/F/1.php">F</a></li>
+          <li><a href="/Letters/G/1.php">G</a></li>
+          <li><a href="/Letters/H/1.php">H</a></li>
+          <li><a href="/Letters/I/1.php">I</a></li>
+          <li><a href="/Letters/J/1.php">J</a></li>
+          <li><a href="/Letters/K/1.php">K</a></li>
+          <li><a href="/Letters/L/1.php">L</a></li>
+          <li><a href="/Letters/M/1.php">M</a></li>
+          <li><a href="/Letters/N/1.php">N</a></li>
+          <li><a href="/Letters/O/1.php">O</a></li>
+          <li><a href="/Letters/P/1.php">P</a></li>
+          <li><a href="/Letters/Q/1.php">Q</a></li>
+          <li><a href="/Letters/R/1.php">R</a></li>
+          <li><a href="/Letters/S/1.php">S</a></li>
+          <li><a href="/Letters/T/1.php">T</a></li>
+          <li><a href="/Letters/U/1.php">U</a></li>
+          <li><a href="/Letters/V/1.php">V</a></li>
+          <li><a href="/Letters/W/1.php">W</a></li>
+          <li><a href="/Letters/X/1.php">X</a></li>
+          <li><a href="/Letters/Y/1.php">Y</a></li>
+          <li><a href="/Letters/Z/1.php">Z</a></li>
+      </ul>
+  </div>
+  <div class="items_glossary"></div>
 </div>
