623ee036e624b78de8e445a86f7a44bd4b1c2fbb
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 05:50:14 +0530

    Update respheader.php

--- assets/Head/respheader.php
+++ assets/Head/respheader.php
@@ -17,39 +17,41 @@
         <div class="menu-primary-menu-container">
             <ul id="main_header" class="resp">
                 <li><a href="https://www.irupc.net" aria-current="page">Home</a></li>
-                <li><a onclick="irupcMoviepgurl(1)">Movies</a>
+                <li><a href="/Movies/1.php"><i class="fas fa-caret-down"></i>Movies</a>
                     <ul class="sub-menu">
-                        <li><a onclick="irupclang('English')">English</a></li>
-                        <li><a onclick="irupclang('Hindi')">Hindi</a></li>
-                        <li><a onclick="irupclang('Tamil')">Tamil</a></li>
-                        <li><a onclick="irupclang('Telugu')">Telugu</a></li>
-                        <li><a onclick="irupclang('Malayalam')">Malayalam</a></li>
-                        <li><a onclick="irupclang('Kannada')">Kannada</a></li>
-                        <li><a onclick="irupclang('Korean')">Korean</a></li>
-                        <li><a onclick="irupclang('Other')">Other</a></li>
+                        <li><a href="/Languages/English/1.php">English</a></li>
+                        <li><a href="/Languages/Hindi/1.php">Hindi</a></li>
+                        <li><a href="/Languages/Tamil/1.php">Tamil</a></li>
+                        <li><a href="/Languages/Telugu/1.php">Telugu</a></li>
+                        <li><a href="/Languages/Malayalam/1.php">Malayalam</a></li>
+                        <li><a href="/Languages/Kannada/1.php">Kannada</a></li>
+                        <li><a href="/Languages/Korean/1.php">Korean</a></li>
+                        <li><a href="/Languages/Other/1.php">Other</a></li>
                     </ul>
                 </li>
-                <li><a onclick="irupctvpgurl(1)">TV Shows</a></li>
+                <li><a href="/assets/php/TVShows.php">TV Shows</a></li>
                 <li><a>Genres</a>
                     <ul class="sub-menu">
-                        <li><a onclick="irupcgenre('Action')">Action</a></li>
-                        <li><a onclick="irupcgenre('Adventure')">Adventure</a></li>
-                        <li><a onclick="irupcgenre('Romance')">Romance</a></li>
-                        <li><a onclick="irupcgenre('Crime')">Crime</a></li>
-                        <li><a onclick="irupcgenre('Comedy')">Comedy</a></li>
-                        <li><a onclick="irupcgenre('Drama')">Drama</a></li>
-                        <li><a onclick="irupcgenre('Horror')">Horror</a></li>
-                        <li><a onclick="irupcgenre('Family')">Family</a></li>
-                        <li><a onclick="irupcgenre('Fantasy')">Fantasy</a></li>
-                        <li><a onclick="irupcgenre('Animation')">Animation</a></li>
-                        <li><a onclick="irupcgenre('Documentary')">Documentary</a></li>
-                        <li><a onclick="irupcgenre('Thriller')">Thriller</a></li>
-                        <li><a onclick="irupcgenre('Sci')">Sci-Fi</a></li>
-                        <li><a onclick="irupcgenre('History')">History</a></li>
+                        <li><a href="/Genres/Action/1.php">Action</a></li>
+                        <li><a href="/Genres/Adventure/1.php">Adventure</a></li>
+                        <li><a href="/Genres/Romance/1.php">Romance</a></li>
+                        <li><a href="/Genres/Crime/1.php">Crime</a></li>
+                        <li><a href="/Genres/Comedy/1.php">Comedy</a></li>
+                        <li><a href="/Genres/Drama/1.php">Drama</a></li>
+                        <li><a href="/Genres/Horror/1.php">Horror</a></li>
+                        <li><a href="/Genres/Family/1.php">Family</a></li>
+                        <li><a href="/Genres/Fantasy/1.php">Fantasy</a></li>
+                        <li><a href="/Genres/Animation/1.php">Animation</a></li>
+                        <li><a href="/Genres/Documentary/1.php">Documentary</a></li>
+                        <li><a href="/Genres/Thriller/1.php">Thriller</a></li>
+                        <li><a href="/Genres/Mystery/1.php">Mystery-Fi</a></li>
+                        <li><a href="/Genres/Sci/1.php">Sci</a></li>
+                        <li><a href="/Genres/History/1.php">History</a></li>
                     </ul>
                 </li>
-                <li><a onclick="ipMovieCollections()">Movie Collections</a></li>
-                <li><a onclick="irupccontact()">Contact US</a></li>
+                <li><a href="/Collections/index.php">Movie Collections</a></li>
+                <li><a href="/assets/php/ContactUs.php">Contact US</a></li>
+                <li><a href="/assets/php/RequestMovies.php">Request Movies</a></li>
             </ul>
         </div>
     </div>
