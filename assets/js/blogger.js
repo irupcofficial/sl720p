8891bc5aa809def8bf276a4c0420d321b32badf1
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 15:11:54 +0530

    Update blogger.js

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -8,22 +8,22 @@
   document.getElementById("dlWC1").innerHTML = "Download Now";
   document.getElementById("dlWC2").innerHTML = "Download Now";
 
-function ins(){ document.getElementById("numberOfSeason").innerHTML = " 00"; document.getElementById('ins').style.display='block';}
-function season01(){ document.getElementById("numberOfSeason").innerHTML = " 01"; document.getElementById('irupcS01').style.display='block';}
-function season02(){ document.getElementById("numberOfSeason").innerHTML = " 02"; document.getElementById('irupcS02').style.display='block';}
-function season03(){ document.getElementById("numberOfSeason").innerHTML = " 03"; document.getElementById('irupcS03').style.display='block';}
-function season04(){ document.getElementById("numberOfSeason").innerHTML = " 04"; document.getElementById('irupcS04').style.display='block';}
-function season05(){ document.getElementById("numberOfSeason").innerHTML = " 05"; document.getElementById('irupcS05').style.display='block';}
-function season06(){ document.getElementById("numberOfSeason").innerHTML = " 06"; document.getElementById('irupcS06').style.display='block';}       
-function season07(){ document.getElementById("numberOfSeason").innerHTML = " 07"; document.getElementById('irupcS07').style.display='block';}
-function season08(){ document.getElementById("numberOfSeason").innerHTML = " 08"; document.getElementById('irupcS08').style.display='block';}
-function season09(){ document.getElementById("numberOfSeason").innerHTML = " 09"; document.getElementById('irupcS09').style.display='block';}
-function season10(){ document.getElementById("numberOfSeason").innerHTML = " 10"; document.getElementById('irupcS10').style.display='block';}
-function season11(){ document.getElementById("numberOfSeason").innerHTML = " 11"; document.getElementById('irupcS11').style.display='block';}
-function season12(){ document.getElementById("numberOfSeason").innerHTML = " 12"; document.getElementById('irupcS12').style.display='block';}
-function season13(){ document.getElementById("numberOfSeason").innerHTML = " 13"; document.getElementById('irupcS13').style.display='block';}
-function season14(){ document.getElementById("numberOfSeason").innerHTML = " 14"; document.getElementById('irupcS14').style.display='block';}
-function season15(){ document.getElementById("numberOfSeason").innerHTML = " 15"; document.getElementById('irupcS15').style.display='block';}
+function ins(){ document.getElementById("numberOfSeason").innerHTML = " 00";}
+function season01(){ document.getElementById("numberOfSeason").innerHTML = " 01";}
+function season02(){ document.getElementById("numberOfSeason").innerHTML = " 02";}
+function season03(){ document.getElementById("numberOfSeason").innerHTML = " 03";}
+function season04(){ document.getElementById("numberOfSeason").innerHTML = " 04";}
+function season05(){ document.getElementById("numberOfSeason").innerHTML = " 05";}
+function season06(){ document.getElementById("numberOfSeason").innerHTML = " 06";}       
+function season07(){ document.getElementById("numberOfSeason").innerHTML = " 07";}
+function season08(){ document.getElementById("numberOfSeason").innerHTML = " 08";}
+function season09(){ document.getElementById("numberOfSeason").innerHTML = " 09";}
+function season10(){ document.getElementById("numberOfSeason").innerHTML = " 10";}
+function season11(){ document.getElementById("numberOfSeason").innerHTML = " 11";}
+function season12(){ document.getElementById("numberOfSeason").innerHTML = " 12";}
+function season13(){ document.getElementById("numberOfSeason").innerHTML = " 13";}
+function season14(){ document.getElementById("numberOfSeason").innerHTML = " 14";}
+function season15(){ document.getElementById("numberOfSeason").innerHTML = " 15";}
 
 function s01Info1Id(resolution, size, ripinfo, msg1a, msg2a){ document.getElementById("res").innerHTML = resolution; document.getElementById("Size").innerHTML = size; document.getElementById("Quality").innerHTML = ripinfo;  document.getElementById("headermsg").innerHTML = msg1a; document.getElementById("sheadermsg").innerHTML = msg2a; }
 function unhideSeasons(){ document.getElementById("totalWrapper").style.display='block'; document.getElementById("pgDlBtn1").style.display='none'; }
