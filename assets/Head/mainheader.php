e3363acf9585687a2bd0a1289860c3ee2a7b9488
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 05:42:25 +0530

    Update mainheader.php

--- assets/Head/mainheader.php
+++ assets/Head/mainheader.php
@@ -6,45 +6,45 @@
                 <ul id="main_header" class="main-header">
                     <!--Buttons in header-->
                     <li><a href="https://www.irupc.net" aria-current="page">Home</a></li>
-                    <li><a onclick="irupcMoviepgurl(1)"><i class="fas fa-caret-down"></i>Movies</a>
+                    <li><a href="/Movies/1.php"><i class="fas fa-caret-down"></i>Movies</a>
                         <ul class="sub-menu"><div class="irupcli">
                             ---------------------------------------------------<br><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('English')">English</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Hindi')">Hindi</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Tamil')">Tamil</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Telugu')">Telugu</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Malayalam')">Malayalam</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Kannada')">Kannada</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Tamil')">Tamil</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Korean')">Korean</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupclang('Other')">Other</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/English/1.php">English</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Hindi/1.php">Hindi</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Tamil/1.php">Tamil</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Telugu/1.php">Telugu</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Malayalam/1.php">Malayalam</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Kannada/1.php">Kannada</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Tamil/1.php">Tamil</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Korean/1.php">Korean</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Languages/Other/1.php">Other</m><br></div><br>
                                     ---------------------------------------------------<br><br>
                                 </div></ul>
                             </li>
-                            <li><a onclick="headtv()">TV Shows</a></li>
+                            <li><a href="/assets/php/TVShows.php">TV Shows</a></li>
                             <li><a><i class="fas fa-caret-down"></i>Genres</a>
                                 <ul class="sub-menu">
                                     ---------------------------------------------------<br><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Action')">Action</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Adventure')">Adventure</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Romance')">Romance</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Crime')">Crime</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Comedy')">Comedy</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Drama')">Drama</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Horror')">Horror</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Family')">Family</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Fantasy')">Fantasy</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Animation')">Animation</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Documentary')">Documentary</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Thriller')">Thriller</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Mystery')">Mystery</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('Sci')">Sci-Fi</m><br></div><br>
-                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m onclick="irupcgenre('History')">History</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Action/1.php">Action</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Adventure/1.php">Adventure</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Romance/1.php">Romance</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Crime/1.php">Crime</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Comedy/1.php">Comedy</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Drama/1.php">Drama</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Horror/1.php">Horror</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Family/1.php">Family</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Fantasy/1.php">Fantasy</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Animation/1.php">Animation</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Documentary/1.php">Documentary</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Thriller/1.php">Thriller</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Mystery/1.php">Mystery</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/Sci/1.php">Sci-Fi</m><br></div><br>
+                                    <div class="ipli"><i class="fas fa-caret-right"><div class="ips"></div></i><m href="/Genres/History/1.php">History</m><br></div><br>
                             ---------------------------------------------------<br><br>
                         </ul>
                     </li>
-                    <li><a onclick="ipMovieCollections()">Movie Collections</a></li>
-                    <li><a onclick="irupccontact()">Contact US</a></li>
+                    <li><a href="/Collections/index.php">Movie Collections</a></li>
+                    <li><a href="/assets/php/ContactUs.php">Contact US</a></li>
                 </ul>
             </div>
         </div>
