d11403c8c8ca2268c3636b570d4b3b8b43fec759
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 19:55:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1379,7 +1379,7 @@
             <footer class="main">
                 <div class="fbox">
                     <div class="fcmpbox">
-                        <div class="copy"><b>&copy; 2020 by <a href="https://www.irupc.net/">IruPC</strong></a>. All Rights Reserved.</b></div>
+                        <div class="copy"><b>&copy; 2020 by <strong><a href="https://www.irupc.net/">IruPC</strong></a>. Sri Lanka's No. 1 <strong>Streaming</strong> Zone. All Rights Reserved.</b></div>
                         <span class="top-page">
                             <a id="top-page"><i class="fas fa-angle-double-up"></i></a>
                         </span>
