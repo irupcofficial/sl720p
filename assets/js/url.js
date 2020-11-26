567690ca2bf0253c8f3210de1d5a5e30a1530705
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 23:28:03 +0530

    Update url.js

--- assets/js/url.js
+++ assets/js/url.js
@@ -38,7 +38,32 @@
             window.open("https://movieslk.herokuapp.com/Letters/"+Letter+"/"+pgno+".php")
         }
 
-        totalmoviepgs=20;
-        totaltvpgs=3;
-        ipNoOfMovies=370;
-        totalActionpgs=11;
+totalmoviepgs=20;
+totaltvpgs=3;
+ipNoOfMovies=370;
+totalActionpgs=11;
+totalTelugupgs=2;
+totalHindipgs=2;
+totalTamilpgs=3;
+totalKannadapgs=1;
+totalKoreanpgs=1;
+totalOtherpgs=1;
+
+totalHistorypgs=1;
+totalActionpgs=11;
+totalAdventurepgs=6;
+totalRomancepgs=3;
+totalCrimepgs=3;
+totalComedypgs=4;
+totalDramapgs=9;
+totalHorrorpgs=3;
+totalFamilypgs=2;
+totalFantasypgs=3;
+totalAnimationpgs=1;
+totalDocumentarypgs=1;
+totalThrillerpgs=5;
+totalMysterypgs=2;
+totalMysterypgs=2;
+
+
+
