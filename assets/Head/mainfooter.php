b3b2b8edec6609734d14310d687dd18bc8c109bf
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-26 05:53:14 +0530

    Update mainfooter.php

--- assets/Head/mainfooter.php
+++ assets/Head/mainfooter.php
@@ -1,15 +1,15 @@
 <footer class="main">
     <div class="fbox">
         <div class="fcmpbox">
-            <div class="copy"><b>&copy; 2020 by <a href="https://www.irupc.net/">IruPC</strong></a>. All Rights Reserved.</b></div>
+            <div class="copy"><b>&copy; 2020 by <strong><a href="https://www.irupc.net/">IruPC</strong></a>. Sri Lanka's No. 1 <strong>Streaming</strong> Zone. All Rights Reserved.</b></div>
             <span class="top-page">
                 <a id="top-page"><i class="fas fa-angle-double-up"></i></a>
             </span>
             <div class="fmenu">
                 <ul id="menu-footer" class="menu">
-                    <li><a onclick="ipMovieCollections()">Movie Collections</a></li>
-                    <li><a onclick="irupccontact()">Contact Us</a></li>
-                    <li><a onclick="irupcrequest()">Request Movies</a></li>
+                    <li><a href="/Collections/index.php">Movie Collections</a></li>
+                    <li><a href="/assets/php/ContactUs.php">Contact US</a></li>
+                    <li><a href="/assets/php/RequestMovies.php">Request Movies</a></li>
                 </ul>
             </div>
         </div>
