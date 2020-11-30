f61917d6575d53818bdbbfe070fcf2e0d4e0512b
Irupc <68991705+Irupc@users.noreply.github.com>
2020-11-30 23:13:48 +0530

    Create Peaky.Blinders_4622.js

---
+++ assets/TV/Peaky.Blinders_4622.js
@@ -0,0 +1,31 @@
+lastRes = "720p"; lastSize = "239MB"; lastRip = "BluRay"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~200MB"; s01Rip = "BluRay"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~150MB"; s02Rip = "BluRay"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s03Res = "720p"; s03Size = "~170MB"; s03Rip = "BluRay"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s04Res = "720p"; s04Size = "~225MB"; s04Rip = "BluRay"; s04Msg1 = "Warning! This is x265 (HEVC) Codec"; s04Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s05Res = "720p"; s05Size = "~220MB"; s05Rip = "BluRay"; s05Msg1 = "Warning! This is x265 (HEVC) Codec"; s05Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://irupc-net.herokuapp.com/1860134804572/@CC.%20S01E01.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s01e02x = "https://irupc-net.herokuapp.com/1877314673756/@CC.%20S01E02.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s01e03x = "https://irupc-net.herokuapp.com/1894494542940/@CC.%20S01E03.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s01e04x = "https://irupc-net.herokuapp.com/1911674412124/@CC.%20S01E04.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s01e05x = "https://irupc-net.herokuapp.com/1928854281308/@CC.%20S01E05.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s01e06x = "https://irupc-net.herokuapp.com/1946034150492/@CC.%20S01E06.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv";
+//@@01z@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://irupc-net.herokuapp.com/1653976374364/@CC.%20S02E01.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s02e02x = "https://irupc-net.herokuapp.com/1671156243548/@CC.%20S02E02.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s02e03x = "https://irupc-net.herokuapp.com/1688336112732/@CC.%20S02E03.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s02e04x = "https://irupc-net.herokuapp.com/1705515981916/@CC.%20S02E04.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s02e05x = "https://irupc-net.herokuapp.com/1722695851100/@CC.%20S02E05.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s02e06x = "https://irupc-net.herokuapp.com/1739875720284/@CC.%20S02E06.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv";
+//@@02z@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://irupc-net.herokuapp.com/1447817944156/@CC.%20S03E01.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s03e02x = "https://irupc-net.herokuapp.com/1464997813340/@CC.%20S03E02.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s03e03x = "https://irupc-net.herokuapp.com/1482177682524/@CC.%20S03E03.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s03e04x = "https://irupc-net.herokuapp.com/1499357551708/@CC.%20S03E04.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s03e05x = "https://irupc-net.herokuapp.com/1516537420892/@CC.%20S03E05.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv"; s03e06x = "https://irupc-net.herokuapp.com/1533717290076/@CC.%20S03E06.Peaky.Blinders.720p.BluRay.x265.HET.Farda.DL.mkv";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@";
+
+//Season 4 URL
+s04e01x = "https://irupc-net.herokuapp.com/228047232092/%5BML%5DS04E01.Peaky.Blinders.720p.H265.AAC.mp4"; s04e02x = "https://irupc-net.herokuapp.com/245227101276/%5BML%5DS04E02.Peaky.Blinders.720p.H265.AAC.mp4"; s04e03x = "https://irupc-net.herokuapp.com/262406970460/%5BML%5DS04E03.Peaky.Blinders.720p.H265.AAC.mp4"; s04e04x = "https://irupc-net.herokuapp.com/279586839644/%5BML%5DS04E04.Peaky.Blinders.720p.H265.AAC.mp4"; s04e05x = "https://irupc-net.herokuapp.com/296766708828/%5BML%5DS04E05.Peaky.Blinders.720p.H265.AAC.mp4"; s04e06x = "https://irupc-net.herokuapp.com/313946578012/%5BML%5DS04E06.Peaky.Blinders.720p.H265.AAC.mp4";
+//@@04z@@  
+s04e01y = "@@04y@@"; s04e02y = "@@04y@@"; s04e03y = "@@04y@@"; s04e04y = "@@04y@@"; s04e05y = "@@04y@@"; s04e06y = "@@04y@@";
+
+//Season 5 URL
+s05e01x = "https://irupc-net.herokuapp.com/1241659513948/%5BML%5DS05E01.Peaky.Blinders.720p.H265.AAC.mp4"; s05e02x = "https://irupc-net.herokuapp.com/1258839383132/%5BML%5DS05E02.Peaky.Blinders.720p.H265.AAC.mp4"; s05e03x = "https://irupc-net.herokuapp.com/1276019252316/%5BML%5DS05E03.Peaky.Blinders.720p.H265.AAC.mp4"; s05e04x = "https://irupc-net.herokuapp.com/1293199121500/%5BML%5DS05E04.Peaky.Blinders.720p.H265.AAC.mp4"; s05e05x = "https://irupc-net.herokuapp.com/1310378990684/%5BML%5DS05E05.Peaky.Blinders.720p.H265.AAC.mp4"; s05e06x = "https://irupc-net.herokuapp.com/1327558859868/%5BML%5DS05E06.Peaky.Blinders.720p.H265.AAC.mp4";
+//@@05z@@ 
+s05e01y = "@@05y@@"; s05e02y = "@@05y@@"; s05e03y = "@@05y@@"; s05e04y = "@@05y@@"; s05e05y = "@@05y@@"; s05e06y = "@@05y@@";
