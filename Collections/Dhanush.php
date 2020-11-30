063978a2f2ba5d933ebb1ad1a5ec16749143d259
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-01 03:11:16 +0530

    Update Dhanush.php

--- Collections/Dhanush.php
+++ Collections/Dhanush.php
@@ -87,7 +87,7 @@
             <div class="module">
                 <div class="content right">
                 <header class="archive_post">
-                        <h1>Rashmika Mandanna Collection</h1><span>9</span>
+                        <h1>Dhanush Collection</h1><span>9</span>
                     </header>
                         <div id="archive-content" class="animation-2 items">
                         <article class="item se seasons">
