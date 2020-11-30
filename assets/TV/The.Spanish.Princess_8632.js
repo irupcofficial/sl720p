856761e68bae51d2effa51479a1356c153fbd96e
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-01 01:47:41 +0530

    Create The.Spanish.Princess_8632.js

---
+++ assets/TV/The.Spanish.Princess_8632.js
@@ -0,0 +1,13 @@
+lastRes = "720p"; lastSize = "397MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~556MB"; s01Rip = "Web-DL"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~400MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://irupc-net.herokuapp.com/168779743796316/The.Spanish.Princess.S01E01.Web.@intermedia.mp4"; s01e02x = "https://irupc-net.herokuapp.com/168796923665500/The.Spanish.Princess.S01E02.Web.@intermedia.mp4"; s01e03x = "https://irupc-net.herokuapp.com/168814103534684/The.Spanish.Princess.S01E03.Web.@intermedia.mp4"; s01e04x = "https://irupc-net.herokuapp.com/168831283403868/The.Spanish.Princess.S01E04.Web.@intermedia.mp4"; s01e05x = "https://irupc-net.herokuapp.com/168848463273052/The.Spanish.Princess.S01E05.Web.@intermedia.mp4"; s01e06x = "https://irupc-net.herokuapp.com/168865643142236/The.Spanish.Princess.S01E06.Web.@intermedia.mp4"; s01e07x = "https://irupc-net.herokuapp.com/168882823011420/The.Spanish.Princess.S01E07.Web.@intermedia.mp4"; s01e08x = "https://irupc-net.herokuapp.com/168900002880604/The.Spanish.Princess.S01E08.Web.@intermedia.mp4";
+//@@01z@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://irupc-net.herokuapp.com/169054621703260/www.irupc.net_The.Spanish.Princess.S02E01.720p.WEB.x265.mkv"; s02e02x = "https://irupc-net.herokuapp.com/169088981441628/www.irupc.net_The.Spanish.Princess.S02E02.720p.WEB.x265.mkv";
+//@@02z@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@";
