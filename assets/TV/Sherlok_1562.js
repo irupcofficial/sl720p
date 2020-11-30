6b655d52307b7c2cc58caf218bd2c01555856a60
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-01 00:52:19 +0530

    Create Sherlok_1562.js

---
+++ assets/TV/Sherlok_1562.js
@@ -0,0 +1,25 @@
+lastRes = "720p"; lastSize = "748MB"; lastRip = "BluRay"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~1GB"; s01Rip = "BluRay"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~1GB"; s02Rip = "BluRay"; s02Msg1 = "x264 Codec File"; s02Msg2 = "---";
+s03Res = "720p"; s03Size = "~650MB"; s03Rip = "BluRay"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s04Res = "720p"; s04Size = "~750MB"; s04Rip = "BluRay"; s04Msg1 = "Warning! This is x265 (HEVC) Codec"; s04Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S01/Sherlock_S01E01_A_Study_in_Pink.mkv"; s01e02x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S01/Sherlock_S01E02_The_Blind_Banker.mkv"; s01e03x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S01/Sherlock_S01E03_The_Great_Game.mkv"; 
+//@@01z@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S02/Sherlock_S02E01_A_Scandal_in_Belgravia.mkv"; s02e02x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S02/Sherlock_S02E02_The_Hounds_of_Baskerville.mkv"; s02e03x = "https://download.irupc.xyz/0:/TV.Shows/Sherlock/S02/Sherlock_S02E03_The_Reichenbach_Fall.mkv";  
+//@@02z@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; 
+
+//Season 3 URL
+s03e01x = "https://irupc-net.herokuapp.com/112996708555868/Sherlock.S03E01.The.Empty.Hearse.720p.10bit.BluRay.x265.mkv"; s03e02x = "https://irupc-net.herokuapp.com/113013888425052/Sherlock.S03E02.The.Sign.of%20Three.720p.10bit.BluRay.x265.mkv"; s03e03x = "https://irupc-net.herokuapp.com/113031068294236/Sherlock.S03E03.His.Last.Vow.720p.10bit.BluRay.5.1.x265.mkv";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@";  
+
+//Season 4 URL
+s04e01x = "https://irupc-net.herokuapp.com/112893629340764/Sherlock.S04E01.The.Six.Thatchers.720p.10bit.x265.mkv"; s04e02x = "https://irupc-net.herokuapp.com/112910809209948/Sherlock.S04E02.The.Lying.Detective.720p.10bit.x265.mkv"; s04e03x = "https://irupc-net.herokuapp.com/112927989079132/Sherlock.S04E03.The.Final.Problem.720p.10bit.x265.mkv";
+//@@04z@@  
+s04e01y = "@@04y@@"; s04e02y = "@@04y@@"; s04e03y = "@@04y@@"; 
