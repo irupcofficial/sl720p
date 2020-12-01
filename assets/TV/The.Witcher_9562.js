13ee147d5f2c25bffc6a4de4b6b8f02a5de38b10
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-01 22:28:31 +0530

    Create The.Witcher_9562.js

---
+++ assets/TV/The.Witcher_9562.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "380MB"; lastRip = "WEB-DL"; lastMsg1 = "x265 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~380MB"; s01Rip = "WEB-DL"; s01Msg1 = "x265 Codec File"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/250899518495836/The_Witcher_S01E01_The_Ends_Beginning_720p_NF_WEB_DL_x265_Pahe_in.mkv"; s01e02x = "https://dl.irupc.xyz/250916698365020/The.Witcher.S01E02.Four.Marks.720p.NF.WEB-DL.x265-Pahe.in.mkv"; s01e03x = "https://dl.irupc.xyz/250933878234204/The.Witcher.S01E03.Betrayer.Moon.720p.NF.WEB-DL.x265-Pahe.in.mkv"; s01e04x = "https://dl.irupc.xyz/250951058103388/The_Witcher_S01E04_Of_Banquets_Bastards_and_Burials_720p_NF_WEB.mkv"; s01e05x = "https://dl.irupc.xyz/250968237972572/The_Witcher_S01E05_Bottled_Appetites_720p_NF_WEB_DL_x265_Pahe_in.mkv"; s01e06x = "https://dl.irupc.xyz/251002597710940/The.Witcher.S01E06.Rare.Species.720p.NF.WEB-DL.x265-Pahe.in.mkv"; s01e07x = "https://dl.irupc.xyz/250985417841756/The.Witcher.S01E07.Before.a.Fall.720p.NF.WEB-DL.x265-Pahe.in.mkv"; s01e08x = "https://dl.irupc.xyz/251019777580124/The.Witcher.S01E08.Much.More.720p.NF.WEB-DL.x265-Pahe.in.mkv";
+//@@01z@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
