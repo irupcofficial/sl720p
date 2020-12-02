fa390d6bad56765848b21cc4f20f1664f0108900
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-02 19:07:50 +0530

    Create The.Good.Karma.Hospital.js

---
+++ assets/TV/The.Good.Karma.Hospital.js
@@ -0,0 +1,19 @@
+lastRes = "SD"; lastSize = "239MB"; lastRip = "HDTV"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "SD"; s01Size = "~500MB"; s01Rip = "HDTV"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+s02Res = "SD"; s02Size = "~600MB"; s02Rip = "HDTV"; s02Msg1 = "x264 Codec File"; s02Msg2 = "---";
+s03Res = "SD"; s03Size = "~300MB"; s03Rip = "HDTV"; s03Msg1 = "x264 Codec File"; s03Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/252566852794084/The%20Good%20Karma%20Hospital%20S01E01%20x264%20RB58.mp4"; s01e02x = "https://dl.irupc.xyz/252584032663268/The%20Good%20Karma%20Hospital%20S01E02%20x264%20RB58.mp4"; s01e03x = "https://dl.irupc.xyz/252601212532452/The%20Good%20Karma%20Hospital%20S01E03%20x264%20RB58.mp4"; s01e04x = "https://dl.irupc.xyz/252618392401636/The%20Good%20Karma%20Hospital%20S01E04%20x264%20RB58.mp4"; s01e05x = "https://dl.irupc.xyz/252635572270820/The%20Good%20Karma%20Hospital%20S01E05%20x264%20RB58.mp4"; s01e06x = "https://dl.irupc.xyz/252652752140004/The%20Good%20Karma%20Hospital%20S01E06%20x264%20RB58.mp4";
+//@@01z@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/252309154756324/The%20Good%20Karma%20Hospital%20S02E01%20Episode%201.mkv"; s02e02x = ""; s02e03x = "https://dl.irupc.xyz/252326334625508/The%20Good%20Karma%20Hospital%20S02E03%20Episode%203.mkv"; s02e04x = ""; s02e05x = "https://dl.irupc.xyz/252343514494692/The%20Good%20Karma%20Hospital%20S02E05%20Episode%205.mkv"; s02e06x = "https://dl.irupc.xyz/252360694363876/The%20Good%20Karma%20Hospital%20S02E06%20Episode%206.mkv";
+//@@02z@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://dl.irupc.xyz/252515313186532/The.Good.Karma.Hospital.S03E01.480p.x264_mSD%5Beztv%5D.mkv"; s03e02x = "https://dl.irupc.xyz/252498133317348/The.Good.Karma.Hospital.S03E02.480p.x264-mSD%5Beztv%5D.mkv"; s03e03x = "https://dl.irupc.xyz/252841730701028/the.good.karma.hospital.s03e03.hdtv.x264-river%5Beztv%5D.mkv"; s03e04x = ""; s03e05x = "https://dl.irupc.xyz/252807370962660/The.Good.Karma.Hospital.S03E05.480p.x264-mSD%5Beztv%5D.mkv"; s03e06x = "@@@";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@";
