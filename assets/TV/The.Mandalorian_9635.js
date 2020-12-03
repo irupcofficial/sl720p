c190f200c42d9a9d46f67fac4b70bb36093b7b3a
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-03 14:00:32 +0530

    Update The.Mandalorian_9635.js

--- assets/TV/The.Mandalorian_9635.js
+++ assets/TV/The.Mandalorian_9635.js
@@ -1,41 +1,13 @@
-lastRes = "720p"; lastSize = "400MB"; lastRip = "Web-DL"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
-s01Res = "720p"; s01Size = "~300MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
-s02Res = "720p"; s02Size = "~350MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+lastRes = "720p"; lastSize = "100MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~200MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~200MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
 
 //Season 1 URL
-s01e01x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E01.Chapter.1.The.Mandalorian.REPACK.720p.10bit.WEBRip.2CH.x265.HEVC-d.mkv"; 
-s01e02x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E02.Chapter.2.The.Child.REPACK.720p.10bit.WEBRip.2CH.x265.HEVC-y.mkv"; 
-s01e03x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E03.INTERNAL.720p.10bit.WEBRip.2CH.x265.HEVC-f.mkv"; 
-s01e04x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E04.720p.10bit.WEBRip.2CH.x265.HEVC-n.mkv"; 
-s01e05x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E05.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv"; 
-s01e06x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E06.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv"; 
-s01e07x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E07.720p.10bit.WEBRip.2CH.x265.HEVC-y.mkv"; 
-s01e08x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E08.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv";
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E01.Chapter.1.The.Mandalorian.REPACK.720p.10bit.WEBRip.2CH.x265.HEVC-d.mkv"; s01e02x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E02.Chapter.2.The.Child.REPACK.720p.10bit.WEBRip.2CH.x265.HEVC-y.mkv"; s01e03x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E03.INTERNAL.720p.10bit.WEBRip.2CH.x265.HEVC-f.mkv"; s01e04x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E04.720p.10bit.WEBRip.2CH.x265.HEVC-n.mkv"; s01e05x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E05.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv"; s01e06x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E06.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv"; s01e07x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E07.720p.10bit.WEBRip.2CH.x265.HEVC-y.mkv"; s01e08x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S01/www.irupc.net__The.Mandalorian.S01E08.720p.10bit.WEBRip.2CH.x265.HEVC-t.mkv";
 //@@01z@@
-s01e01y = "@@01y@@"; 
-s01e02y = "@@01y@@"; 
-s01e03y = "@@01y@@"; 
-s01e04y = "@@01y@@"; 
-s01e05y = "@@01y@@"; 
-s01e06y = "@@01y@@"; 
-s01e07y = "@@01y@@"; 
-s01e08y = "@@01y@@";
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
 
 //Season 2 URL
-s02e01x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E01.720p.WEB.x265-j.mkv"; 
-s02e02x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E02.720p.WEBRip.x265-v.mkv"; 
-s02e03x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E03.720p.WEB.x265-u.mkv"; 
-s02e04x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian._IruPC.net_S02E04.720p.WEBRip.x265-b.mkv"; 
-s02e05x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E05.720p.WEB.x265-c.mkv"; 
-s02e06x = "@@02x@@"; 
-s02e07x = "@@02x@@"; 
-s02e08x = "@@02x@@";
+s02e01x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E01.720p.WEB.x265-j.mkv"; s02e02x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E02.720p.WEBRip.x265-v.mkv"; s02e03x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E03.720p.WEB.x265-u.mkv"; s02e04x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian._IruPC.net_S02E04.720p.WEBRip.x265-b.mkv"; s02e05x = "https://download.irupc.xyz/0:/TV.Shows/The.Mandalorian/S02/The.Mandalorian_IruPC.net_S02E05.720p.WEB.x265-c.mkv"; s02e06x = "@@02x@@"; s02e07x = "@@02x@@"; s02e08x = "@@02x@@";
 //@@02z@@
-s02e01y = "@@02y@@"; 
-s02e02y = "@@02y@@"; 
-s02e03y = "@@02y@@"; 
-02e04y = "@@02y@@"; 
-s02e05y = "@@02y@@"; 
-s02e06y = "@@02y@@"; 
-s02e07y = "@@02y@@"; 
-s02e08y = "@@02y@@";
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
