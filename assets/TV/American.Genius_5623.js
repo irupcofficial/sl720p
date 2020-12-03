dd66e91ea0210c0236bb65b605f66ecc621b9883
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-03 08:04:45 +0530

    Create American.Genius_5623.js

---
+++ assets/TV/American.Genius_5623.js
@@ -0,0 +1,21 @@
+lastRes = "480p"; lastSize = "400MB"; lastRip = "Web-DL"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "480p"; s01Size = "~300MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Colt.Vs.Wesson-1.m4v"; 
+s01e02x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Edison.Vs.Tesla-2.m4v"; 
+s01e03x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Farnsworth.Vs.Sarnoff-3.m4v"; 
+s01e04x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Hearst.Vs.Pulitzer-4.m4v"; 
+s01e05x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Jobs.Vs.Gates-5.m4v"; 
+s01e06x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Oppenheimer.Vs.Heisenberg-6.m4v"; 
+s01e07x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius.Space.Race-7.m4v"; 
+s01e08x = "https://download.irupc.xyz/0:/TV.Shows/American.Genius/www.irupc.net__American.Genius..Wright.Bros.Vs.Curtiss-8.m4v";
+//@@01z@@
+s01e01y = "https://dl.irupc.xyz/253305587168996/American%20Genius%20%20%20Colt%20Vs%20Wesson-1.m4v"; 
+s01e02y = "https://dl.irupc.xyz/253322767038180/American%20Genius%20%20%20Edison%20Vs%20Tesla-2.m4v"; 
+s01e03y = "https://dl.irupc.xyz/253339946907364/American%20Genius%20%20%20Farnsworth%20Vs%20Sarnoff-3.m4v"; 
+s01e04y = "https://dl.irupc.xyz/253357126776548/American%20Genius%20%20%20Hearst%20Vs%20Pulitzer-4.m4v"; 
+s01e05y = "https://dl.irupc.xyz/253374306645732/American%20Genius%20%20%20Jobs%20Vs%20Gates-5.m4v"; 
+s01e06y = "https://dl.irupc.xyz/253391486514916/American%20Genius%20%20%20Oppenheimer%20Vs%20Heisenberg-6.m4v"; 
+s01e07y = "https://dl.irupc.xyz/253408666384100/American%20Genius%20%20%20Space%20Race-7.m4v"; 
+s01e08y = "https://dl.irupc.xyz/253425846253284/American%20Genius%20%20%20Wright.Bros%20Vs%20Curtiss-8.m4v";
