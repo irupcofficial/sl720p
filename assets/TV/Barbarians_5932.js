c97c5189b31929de453f7c0068e2c6239e84ac1f
Irupc <68991705+Irupc@users.noreply.github.com>
2020-12-06 18:38:05 +0530

    Create Barbarians_5932.js

---
+++ assets/TV/Barbarians_5932.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "457MB"; lastRip = "WebRip"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~430MB"; s01Rip = "WebRip"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://irupc-net.herokuapp.com/194824425479260/Barbarians.2020.S01E01.WEBRip.x264-ION10.mp4"; s01e02x = "https://irupc-net.herokuapp.com/194721346264156/www.irupc.net__Barbarians.2020.S01E02.WEBRip.x264.mp4"; s01e03x = "https://irupc-net.herokuapp.com/194652626787420/www.irupc.net__Barbarians.2020.S01E03.WEBRip.x264.mp4"; s01e04x = "https://irupc-net.herokuapp.com/194669806656604/www.irupc.net__Barbarians.2020.S01E04.WEBRip.x264.mp4"; s01e05x = "https://irupc-net.herokuapp.com/194755706002524/www.irupc.net__Barbarians.2020.S01E05.WEBRip.x264.mp4"; s01e06x = "https://irupc-net.herokuapp.com/194790065740892/www.irupc.net__Barbarians.2020.S01E06.WEBRip.x264.mp4";
+//
+s01e01y = "ZZZ"; s01e02y = "ZZZ"; s01e03y = "ZZZ"; s01e04y = "ZZZ"; s01e05y = "ZZZ"; s01e06y = "ZZZ";
