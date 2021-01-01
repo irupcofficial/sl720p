f3e92f6158dfb6c554b62bcb8153471e366ba1ea
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-01 12:48:44 +0530

    Create Fate.Zero_95623.js

---
+++ assets/TV/Fate.Zero_95623.js
@@ -0,0 +1,17 @@
+lastRes = "720p"; lastSize = "134MB"; lastRip = "---"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "1080p"; s01Size = "~250MB"; s01Rip = "---"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~120MB"; s02Rip = "---"; s02Msg1 = "x264 Codec File"; s02Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/313880918924380/01%20-%20Summoning%20of%20the%20Heroic%20Spirits.mkv"; s01e02x = "https://dl.irupc.xyz/313898098793564/02%20-%20False%20Start.mkv"; s01e03x = "https://dl.irupc.xyz/313915278662748/03%20-%20Land%20of%20Winter%20Trees.mkv"; s01e04x = "https://dl.irupc.xyz/313932458531932/04%20-%20Blade%20of%20the%20Demonic%20Spear.mkv"; s01e05x = "https://dl.irupc.xyz/313949638401116/05%20-%20The%20Vicious%20Beast%20Roars.mkv"; s01e06x = "https://dl.irupc.xyz/313966818270300/06%20-%20Night%20of%20Stratagems.mkv"; s01e07x = "https://dl.irupc.xyz/313983998139484/07%20-%20Forest%20of%20Demons.mkv"; s01e08x = "https://dl.irupc.xyz/314001178008668/08%20-%20The%20Magus%20Killer.mkv"; s01e09x = "https://dl.irupc.xyz/314018357877852/09%20-%20Master%20and%20Servant.mkv"; s01e10x = "https://dl.irupc.xyz/314052717616220/10%20-%20Rin's%20Adventure.mkv";
+s01e11x = "https://dl.irupc.xyz/314035537747036/11%20-%20The%20Grail%20Dialogue.mkv"; s01e12x = "https://dl.irupc.xyz/314069897485404/12%20-%20Invitation%20of%20the%20Holy%20Grail.mkv"; s01e13x = "https://dl.irupc.xyz/314087077354588/13%20-%20The%20Forbidden%20Banquet.mkv";
+//@@01y@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@"; s01e09y = "@@01y@@"; s01e10y = "@@01y@@";
+s01e11y = "@@01y@@"; s01e12y = "@@01y@@"; s01e13y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/314327595523164/14%20-%20The%20Mion%20River%20Battle.mkv"; s02e02x = "https://dl.irupc.xyz/314344775392348/15%20-%20Golden%20Brilliance.mkv"; s02e03x = "https://dl.irupc.xyz/314379135130716/16%20-%20The%20End%20of%20Honor.mkv"; s02e04x = "https://dl.irupc.xyz/314396314999900/17%20-%20The%20Eighth%20Contract.mkv"; s02e05x = "https://dl.irupc.xyz/314361955261532/18%20-%20Distant%20Memories.mkv"; s02e06x = "https://dl.irupc.xyz/314413494869084/19%20-%20Where%20Justice%20is%20Found.mkv"; s02e07x = "https://dl.irupc.xyz/314430674738268/20%20-%20Return%20of%20the%20Assassin.mkv"; s02e08x = "https://dl.irupc.xyz/314447854607452/21%20-%20Knight%20on%20Two%20Wheels.mkv"; s02e09x = "https://dl.irupc.xyz/314465034476636/22%20-%20All%20Evil%20in%20This%20World.mkv"; s02e10x = "https://dl.irupc.xyz/314482214345820/23%20-%20The%20Sea%20at%20the%20End%20of%20the%20World.mkv";
+s02e11x = "https://dl.irupc.xyz/314499394215004/24%20-%20The%20Last%20Command%20Seal.mkv"; s02e12x = "https://dl.irupc.xyz/314516574084188/25%20-%20Fate%20Zero.mkv";
+//@@02y@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@"; s02e09y = "@@02y@@"; s02e10y = "@@02y@@";
+s02e11y = "@@02y@@"; s02e12y = "@@02y@@";
