8e4bc9e9222e827be0c811c483b8eecb29374935
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-02 09:01:44 -0800

    Create Fate.Stay_4875.js

---
+++ assets/TV/Fate.Stay_4875.js
@@ -0,0 +1,17 @@
+lastRes = "720p"; lastSize = "110MB"; lastRip = "---"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~170MB"; s01Rip = "---"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~170MB"; s02Rip = "---"; s02Msg1 = "x264 Codec File"; s02Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/317454331714652/00.%20Prologue.mkv"; s01e02x = "https://dl.irupc.xyz/317471511583836/01.%20A%20Winter%20Day,%20A%20Fateful%20Night.mkv"; s01e03x = "https://dl.irupc.xyz/317488691453020/02%20-%20The%20Curtain%20Goes%20Up.mkv"; s01e04x = "https://dl.irupc.xyz/317505871322204/03.%20The%20First%20Battle.mkv"; s01e05x = "https://dl.irupc.xyz/317523051191388/04.%20Finding%20the%20Will%20to%20Fight.mkv"; s01e06x = "https://dl.irupc.xyz/317540231060572/05%20-%20Dancing%20After%20School.mkv"; s01e07x = "https://dl.irupc.xyz/317557410929756/06%20-%20Mirage.mkv"; s01e08x = "https://dl.irupc.xyz/317574590798940/07.%20The%20Reward%20for%20the%20Death%20Match.mkv"; s01e09x = "https://dl.irupc.xyz/317591770668124/08.%20Winter%20Days,%20Where%20the%20Heart%20Is.mkv"; s01e10x = "https://dl.irupc.xyz/317626130406492/09.%20The%20Distance%20Between%20Them.mkv";
+s01e11x = "https://dl.irupc.xyz/317608950537308/10.%20The%20Fifth%20Contractor.mkv"; s01e12x = "https://dl.irupc.xyz/317660490144860/11.%20A%20Visitor%20Approaches%20Lightly.mkv"; s01e13y = "https://dl.irupc.xyz/317643310275676/12%20-%20The%20Final%20Choice.mkv";
+//@@01y@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@"; s01e09y = "@@01y@@"; s01e10y = "@@01y@@";
+s01e11y = "@@01y@@"; s01e12y = "@@01y@@"; s01e13y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/317694849883228/01.%20Time%20of%20Departure.mkv"; s02e02x = "https://dl.irupc.xyz/317712029752412/02.%20The%20Princess%20of%20Colchis.mkv"; s02e03x = "https://dl.irupc.xyz/317729209621596/03.%20A%20Battle%20of%20Legend.mkv"; s02e04x = "https://dl.irupc.xyz/317746389490780/04.%20Winter%20Days,%20the%20Form%20Wishes%20Take.mkv"; s02e05x = "https://dl.irupc.xyz/317763569359964/05.%20The%20Dark%20Sword%20Bares%20Its%20Fangs.mkv"; s02e06x = "https://dl.irupc.xyz/317780749229148/06.%20The%20Beginning%20of%20the%20Circle.mkv"; s02e07x = "https://dl.irupc.xyz/317797929098332/07.%20Idealism%60s%20End%20(The%20Answer).mkv"; s02e08x = "https://dl.irupc.xyz/317815108967516/08.%20Unlimited%20Blade%20Works.mkv"; s02e09x = "https://dl.irupc.xyz/317832288836700/09.%20Answer.mkv"; s02e10x = "https://dl.irupc.xyz/317849468705884/10.%20Winter%20Days,%20a%20Long%20Way%20Home.mkv";
+s02e11x = "https://dl.irupc.xyz/317866648575068/11.%20Incarnation.mkv"; s02e12x = "https://dl.irupc.xyz/317883828444252/12.%20Unlimited%20Blade%20Works.mkv"; s02e13x = "https://dl.irupc.xyz/317901008313436/13.%20Epilogue.mkv";
+//@@02y@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@"; s02e09y = "@@02y@@"; s02e10y = "@@02y@@";
+s02e11y = "@@02y@@"; s02e12y = "@@02y@@"; s02e13y = "@@02y@@";
