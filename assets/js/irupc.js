b31a0a6363042b14f9af7349cd8c99cffbc23ce7
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-03 09:51:21 +0530

    Update irupc.js

--- assets/js/irupc.js
+++ assets/js/irupc.js
@@ -16,3 +16,21 @@
     function iplang2(){
         document.getElementById('iplang2').style.display='block'; document.getElementById('iplang1').style.display='none';
     }
+
+//FaceBook Messenger
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+      
