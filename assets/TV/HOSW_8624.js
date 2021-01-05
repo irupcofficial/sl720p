a2a373d08c3c749fdc8acf7c9c5c2f0f17ceb2a3
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-05 16:33:17 +0530

    Create HOSW_8624.js

---
+++ assets/TV/HOSW_8624.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "195MB"; lastRip = "WebRip"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~200MB"; s01Rip = "WebRip"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/321130823720028/HOSW%20S01E01%20(2021)%20720p%20@movieMartian1.mkv"; s01e02x = "https://dl.irupc.xyz/321148003589212/HOSW%20S01E02%20(2021)%20720p%20@movieMartian1.mkv"; s01e03x = "https://dl.irupc.xyz/321165183458396/HOSW%20S01E03%20(2021)%20720p%20@movieMartian1.mkv"; s01e04x = "https://dl.irupc.xyz/321182363327580/HOSW%20S01E04%20(2021)%20720p%20@movieMartian1.mkv"; s01e05x = "https://dl.irupc.xyz/321199543196764/HOSW%20S01E05%20(2021)%20720p%20@movieMartian1.mkv"; s01e06x = "https://dl.irupc.xyz/321216723065948/HOSW%20S01E06%20(2021)%20720p%20@movieMartian1.mkv";
+//@@01y@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@";
