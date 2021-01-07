3eacb13b4e3f57fb3e685c0e70b2c668fddc21f1
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-07 23:38:35 +0530

    Create Assassins.Creed.Lineage_8462.js

---
+++ assets/TV/Assassins.Creed.Lineage_8462.js
@@ -0,0 +1,7 @@
+lastRes = "---"; lastSize = "200MB"; lastRip = "---"; lastMsg1 = "Plz Watch Online Before Download"; lastMsg2 = "Because ";
+s01Res = "---"; s01Size = "~200MB"; s01Rip = "---"; s01Msg1 = "Plz Watch Online Before Download"; s01Msg2 = "Because ";
+
+//Season 1 URL
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/Assassins.Creed.Lineage/www.irupc.net__Assassins.Creed.Lineage.Part.1.mp4"; s01e02x = "https://download.irupc.xyz/0:/TV.Shows/Assassins.Creed.Lineage/www.iruPC.net__Assassins.Creed.Lineage.Part.2.mp4"; s01e03x = "https://download.irupc.xyz/0:/TV.Shows/Assassins.Creed.Lineage/www.iruPC.net__Assassins.Creed.Lineage.Part.3.mp4";
+//@@01x@@
+s01e01y = "https://dl.irupc.xyz/323518825536604/Assassin's%20Creed%20-%20Lineage%20(Part%201).mp4"; s01e02y = "https://dl.irupc.xyz/323536005405788/Assassin's%20Creed%20-%20Lineage%20(Part%202).mp4"; s01e03y = "https://dl.irupc.xyz/323553185274972/Assassin's%20Creed%20-%20Lineage%20(Part%203).mp4";
