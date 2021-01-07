70c3d8778c9fc7589465a5a57bbff1d9caa7c8c2
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-07 09:30:19 +0530

    Create Surviving.Death_8631.js

---
+++ assets/TV/Surviving.Death_8631.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "504MB"; lastRip = "WebRip"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~500MB"; s01Rip = "WebRip"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/322694191815772/SURVIVING%20DEATH%20S01E01%20(2021)%20720p%20@movieMartian1.mp4"; s01e02x = "https://dl.irupc.xyz/322711371684956/SURVIVING%20DEATH%20S01E02%20(2021)%20720p%20@movieMartian1.mp4"; s01e03x = "https://dl.irupc.xyz/322728551554140/SURVIVING%20DEATH%20S01E03%20(2021)%20720p%20@movieMartian1.mp4"; s01e04x = "https://dl.irupc.xyz/322745731423324/SURVIVING%20DEATH%20S01E04%20(2021)%20720p%20@movieMartian1.mp4"; s01e05x = "https://dl.irupc.xyz/322762911292508/SURVIVING%20DEATH%20S01E05%20(2021)%20720p%20@movieMartian1.mp4"; s01e06x = "https://dl.irupc.xyz/322780091161692/SURVIVING%20DEATH%20S01E06%20(2021)%20720p%20@movieMartian1.mp4";
+//@@01y@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@";
