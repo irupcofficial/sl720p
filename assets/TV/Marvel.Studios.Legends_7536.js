33036daa902f7b2be6f2987d14bb6899f7076242
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-09 09:51:15 +0530

    Create Marvel.Studios.Legends_7536.js

---
+++ assets/TV/Marvel.Studios.Legends_7536.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "35MB"; lastRip = "HDRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~39MB"; s01Rip = "HDRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/Marvel.Studios.Legends/www.irupc.net__Marvel.Studios.Legends.2021.x265.S01E01.HDRip.mkv"; s01e02x = "https://download.irupc.xyz/0:/TV.Shows/Marvel.Studios.Legends/www.irupc.net__Marvel.Studios.Legends.2021.x265.S01E02.HDRip.mkv";
+//@@01x@@
+s01e01y = "https://dl.irupc.xyz/324961934548060/www.irupc.net_Marvel.Studios.Legends.2021.x265.S01E01.HDRip.mkv"; s01e02y = "https://dl.irupc.xyz/324996294286428/www_irupc_net_Marvel_Studios_Legends_2021_x265_S01E02_HDRip_x_matroska.mkv";
