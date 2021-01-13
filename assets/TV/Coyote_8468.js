6f83d9e8cbe976bc4355b10d63eecca252143a2b
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-13 06:00:36 +0530

    Create Coyote_8468.js

---
+++ assets/TV/Coyote_8468.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "280MB"; lastRip = "WebRip"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~350MB"; s01Rip = "WebRip"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E01.720p.WEBRip.x264-f.mkv"; s01e02x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E02.720p.WEBRip.x264-q.mkv"; s01e03x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E03.720p.WEBRip.x264-x.mkv"; s01e04x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E04.720p.WEBRip.x264-i.mkv"; s01e05x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E05.720p.WEBRip.x264-p.mkv"; s01e06x = "https://download.irupc.xyz/0:/TV.Shows/Coyote/S01/Coyote.S01E06.720p.WEBRip.x264-r.mkv";
+//
+s01e01y = "https://dl.irupc.xyz/325408611146844/Coyote.S01E01.720p.WEBRip.x264-GalaxyTV.mkv"; s01e02y = "https://dl.irupc.xyz/325425791016028/Coyote.S01E02.720p.WEBRip.x264-GalaxyTV.mkv"; s01e03y = "https://dl.irupc.xyz/325442970885212/Coyote.S01E03.720p.WEBRip.x264-GalaxyTV.mkv"; s01e04y = "https://dl.irupc.xyz/325460150754396/Coyote.S01E04.720p.WEBRip.x264-GalaxyTV.mkv"; s01e05y = "https://dl.irupc.xyz/325477330623580/Coyote.S01E05.720p.WEBRip.x264-GalaxyTV.mkv"; s01e06y = "https://dl.irupc.xyz/325494510492764/Coyote.S01E06.720p.WEBRip.x264-GalaxyTV.mkv";
