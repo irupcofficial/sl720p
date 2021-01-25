a8023473ffbf1cc7c744e4e088e02699d1ffadf1
Irupc <68991705+Irupc@users.noreply.github.com>
2021-01-25 21:38:59 +0530

    Create American.Gods_8963.js

---
+++ assets/TV/American.Gods_8963.js
@@ -0,0 +1,25 @@
+lastRes = "720p"; lastSize = "346MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~250MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~250MB"; s02Rip = "WebRip"; s02Msg1 = "x264 Codec File"; s02Msg2 = "---";
+s03Res = "720p"; s03Size = "~250MB"; s03Rip = "WebRip"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 3 URL
+s01e01x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E01.720p.WEBRip.x265.HEVC.s.mkv"; s01e02x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E02.720p.WEBRip.x265.HEVC.b.mkv"; s01e03x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E03.720p.WEBRip.x265.HEVC.vb.mkv"; s01e04x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E04.720p.WEBRip.x265.HEVC.vb.mkv"; s01e05x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E05.720p.WEBRip.x265.HEVC.x.mkv"; s01e06x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E06.720p.WEBRip.x265.HEVC.bb.mkv"; s01e07x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E07.720p.WEBRip.x265.HEVC.q.mkv"; s01e08x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S01/American.Gods.S01E08.720p.WEBRip.x265.HEVC.p.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E01.720p.WEBRip.x265.mkv"; s02e02x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E02.720p.WEB.x265-q.mkv"; s02e03x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E03.720p.WEB.x265-p.mkv"; s02e04x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E04.720p.WEB.x265-o.mkv"; s02e05x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E05.720p.WEB.x265-a.mkv"; s02e06x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E06.720p.WEB.x265-b.mkv"; s02e07x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E07.720p.WEB.x265-g.mkv"; s02e08x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S02/American.Gods.S02E08.720p.WEB.x265-b.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S03/American.Gods.S03E01.720p.WEB.x265-h.mkv"; s03e02x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S03/American.Gods.S03E02.720p.WEB.x265-op.mkv"; s03e03x = "https://cloud.irupc.xyz/0:/TV.Shows/American.Gods/S03/American.Gods.S03E03.720p.WEB.x265-pl.mkv"; 
+//s03e04x = "@@03x@@"; s03e05x = "@@03x@@"; s03e06x = "@@03x@@"; s03e07x = "@@03x@@"; s03e08x = "@@03x@@"; s03e09x = "@@03x@@"; s03e10x = "@@03x@@";
+//s03e11x = "@@03x@@"; s03e12x = "@@03x@@"; s03e13x = "@@03x@@"; s03e14x = "@@03x@@"; s03e15x = "@@03x@@"; s03e16x = "@@03x@@"; s03e17x = "@@03x@@"; s03e18x = "@@03x@@"; s03e19x = "@@03x@@"; s03e20x = "@@03x@@";
+//s03e21x = "@@03x@@"; s03e22x = "@@03x@@"; s03e23x = "@@03x@@"; s03e24x = "@@03x@@"; s03e25x = "@@03x@@"; s03e26x = "@@03x@@"; s03e27x = "@@03x@@"; s03e28x = "@@03x@@"; s03e29x = "@@03x@@"; s03e30x = "@@03x@@";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; 
+//s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@"; s03e07y = "@@03y@@"; s03e08y = "@@03y@@"; s03e09y = "@@03y@@"; s03e10y = "@@03y@@";
+//s03e11y = "@@03y@@"; s03e12y = "@@03y@@"; s03e13y = "@@03y@@"; s03e14y = "@@03y@@"; s03e15y = "@@03y@@"; s03e16y = "@@03y@@"; s03e17y = "@@03y@@"; s03e18y = "@@03y@@"; s03e19y = "@@03y@@"; s03e20y = "@@03y@@";
+//s03e21y = "@@03y@@"; s03e22y = "@@03y@@"; s03e23y = "@@03y@@"; s03e24y = "@@03y@@"; s03e25y = "@@03y@@"; s03e26y = "@@03y@@"; s03e27y = "@@03y@@"; s03e28y = "@@03y@@"; s03e29y = "@@03y@@"; s03e30y = "@@03y@@";
