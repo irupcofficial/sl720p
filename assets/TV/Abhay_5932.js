8dfc525628f3ab0d9802ee6d4c15ed83cdf0e7f7
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-06 16:37:45 +0530

    Create Abhay_5932.js

---
+++ assets/TV/Abhay_5932.js
@@ -0,0 +1,13 @@
+lastRes = "720p"; lastSize = "212MB"; lastRip = "WebDL"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~190MB"; s01Rip = "WebDL"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~200MB"; s02Rip = "WebDL"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/391121610775644/Abhay.S01E01.Hindi.720p.Downloadhub.host.mkv"; s01e02x = "https://dl.irupc.xyz/391138790644828/Abhay.S01E02.Hindi.720p.Downloadhub.host.mkv"; s01e03x = "https://dl.irupc.xyz/391155970514012/Abhay.S01E03.Hindi.720p.Downloadhub.host.mkv"; s01e04x = "https://dl.irupc.xyz/391173150383196/Abhay.S01E04.Hindi.720p.Downloadhub.host.mkv"; s01e05x = "https://dl.irupc.xyz/391190330252380/Abhay.S01E05.Hindi.720p.Downloadhub.host.mkv"; s01e06x = "https://dl.irupc.xyz/391207510121564/Abhay.S01E06.Hindi.720p.Downloadhub.host.mkv"; s01e07x = "https://dl.irupc.xyz/391224689990748/Abhay.S01E07.Hindi.720p.Downloadhub.host.mkv"; s01e08x = "https://dl.irupc.xyz/391241869859932/Abhay.S01E08.Hindi.720p.Downloadhub.host.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/391396488682588/Abhay.S02.E01.Hindi.720p.HEVC.WEB-DL.AAC.x265-HDHub4u.NL.mkv"; s02e02x = "https://dl.irupc.xyz/391413668551772/Abhay.S02.E02.Hindi.720p.HEVC.WEB-DL.AAC.x265-HDHub4u.NL.mkv"; s02e03x = "https://dl.irupc.xyz/391430848420956/Abhay.S02.E03.Hindi.720p.HEVC.WEB-DL.AAC.x265-HDHub4u.NL.mkv"; s02e04x = "https://dl.irupc.xyz/391448028290140/Abhay.S02.E04.Hindi.720p.HEVC.WEB-DL.AAC.x265-HDHub4u.NL.mkv"; s02e05x = "https://dl.irupc.xyz/391465208159324/Abhay.S02.E05.Hindi.720p.HEVC.WEB-DL.AAC.x265-HDHub4u.NL.mkv"; s02e06x = "https://dl.irupc.xyz/391482388028508/Abhay.S02.E06.Hindi.720p.HEVC.WEB-DL.x265.ESub-HDHub4u.NL.mkv"; s02e07x = "https://dl.irupc.xyz/391499567897692/Abhay.S02.E07.Hindi.720p.HEVC.WEB-DL.x265.ESub-HDHub4u.NL.mkv"; s02e08x = "https://dl.irupc.xyz/391516747766876/Abhay.S02.E08.Hindi.720p.HEVC.WEB-DL.x265.ESub-HDHub4u.NL.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
