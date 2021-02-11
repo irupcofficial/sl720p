64792148f6039ae39b720e1018cfbde7d2c3f4e2
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-11 21:45:59 +0530

    Create Generation.Kill_1392.js

---
+++ assets/TV/Generation.Kill_1392.js
@@ -0,0 +1,7 @@
+lastRes = "720p"; lastSize = "826MB"; lastRip = "BluRay"; lastMsg1 = "x264 Codec Movie"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~800MB"; s01Rip = "BluRay"; s01Msg1 = "x264 Codec Movie"; s01Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/402305705614428/Generation.Kill.2008.S01E01.720p.BluRay.x264-@iMediaShare.mp4"; s01e02x = "https://dl.irupc.xyz/402340065352796/Generation.Kill.2008.S01E02.720p.BluRay.x264-@iMediaShare.mp4"; s01e03x = "https://dl.irupc.xyz/402357245221980/Generation.Kill.2008.S01E03.720p.BluRay.x264-@iMediaShare.mp4"; s01e04x = "https://dl.irupc.xyz/402374425091164/Generation.Kill.2008.S01E04.720p.BluRay.x264-@iMediaShare.mp4"; s01e05x = "https://dl.irupc.xyz/402322885483612/Generation.Kill.2008.S01E05.720p.BluRay.x264-@iMediaShare.mp4"; s01e06x = "https://dl.irupc.xyz/402408784829532/Generation.Kill.2008.S01E06.720p.BluRay.x264-@iMediaShare.mp4"; s01e07x = "https://dl.irupc.xyz/402391604960348/Generation.Kill.2008.S01E07.720p.BluRay.x264-@iMediaShare.mp4";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@";
