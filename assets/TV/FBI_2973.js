36c51627c28a9ca3a3566e41a40a13d31dc324c6
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-13 13:16:14 +0530

    Create FBI_2973.js

---
+++ assets/TV/FBI_2973.js
@@ -0,0 +1,30 @@
+s01Res = "480p"; s01Size = "~180MB"; s01Rip = "HDTV"; s01Msg1 = "x264 Codec Movie"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~200MB"; s02Rip = "HDTV"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s03Res = "720p"; s03Size = "~230MB"; s03Rip = "HDTV"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s08Res = "480p"; s08Size = "~180MB"; s08Rip = "---"; s08Msg1 = "x264 Codec Movie"; s08Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/404401649654876/FBI.S01E01.480p.HDTV.x264.mkv"; s01e02x = "https://dl.irupc.xyz/404418829524060/FBI.S01E02.480p.HDTV.x264.mkv"; s01e03x = "https://dl.irupc.xyz/404436009393244/FBI.S01E03.480p.HDTV.x264.mkv"; s01e04x = "https://dl.irupc.xyz/404453189262428/FBI.S01E04.480p.HDTV.x264.mkv"; s01e05x = "https://dl.irupc.xyz/404470369131612/FBI.S01E05.480p.HDTV.x264.mkv"; s01e06x = "https://dl.irupc.xyz/404487549000796/FBI.S01E06.480p.HDTV.x264.mkv"; s01e07x = "https://dl.irupc.xyz/404521908739164/FBI.S01E07.480p.HDTV.x264.mkv"; s01e08x = "https://dl.irupc.xyz/404504728869980/FBI.S01E08.480p.HDTV.x264.mkv"; s01e09x = "https://dl.irupc.xyz/404539088608348/FBI.S01E09.480p.HDTV.x264.mkv"; s01e10x = "https://dl.irupc.xyz/404556268477532/FBI.S01E10.480p.HDTV.x264.mkv";
+s01e11x = "https://dl.irupc.xyz/404642167823452/FBI.S01E11.480p.WEB-DL.x264.mkv"; s01e12x = "https://dl.irupc.xyz/404573448346716/FBI.S01E12.480p.HDTV.x264.mkv"; s01e13x = "https://dl.irupc.xyz/404590628215900/FBI.S01E13.480p.HDTV.x264.mkv"; s01e14x = "https://dl.irupc.xyz/404607808085084/FBI.S01E14.480p.HDTV.x264.mkv"; s01e15x = "https://dl.irupc.xyz/404624987954268/FBI.S01E15.480p.HDTV.x264.mkv"; s01e16x = "https://dl.irupc.xyz/404659347692636/FBI.S01E16.480p.HDTV.x264.mkv"; s01e17x = "https://dl.irupc.xyz/404676527561820/FBI.S01E17.480p.WEB-DL.x264.mkv"; s01e18x = "https://dl.irupc.xyz/404693707431004/FBI.S01E18.480p.HDTV.x264.mkv"; s01e19x = "https://dl.irupc.xyz/404710887300188/FBI.S01E19.480p.HDTV.x264.mkv"; s01e20x = "https://dl.irupc.xyz/404728067169372/FBI.S01E20.480p.HDTV.x264.mkv";
+s01e21x = "https://dl.irupc.xyz/404745247038556/FBI.S01E21.480p.HDTV.x264.mkv"; s01e22x = "https://dl.irupc.xyz/404762426907740/FBI.S01E22.480p.HDTV.x264.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@"; s01e09y = "@@01y@@"; s01e10y = "@@01y@@";
+s01e11y = "@@01y@@"; s01e12y = "@@01y@@"; s01e13y = "@@01y@@"; s01e14y = "@@01y@@"; s01e15y = "@@01y@@"; s01e16y = "@@01y@@"; s01e17y = "@@01y@@"; s01e18y = "@@01y@@"; s01e19y = "@@01y@@"; s01e20y = "@@01y@@";
+s01e21y = "@@01y@@"; s01e22y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/405174743768156/FBI.S02E01.720p.HDTV.x265%5Bsmtv%5D.mkv"; s02e02x = "https://dl.irupc.xyz/405157563898972/FBI.S02E02.720p.HDTV.x265%5Bsmtv%5D.mkv"; s02e03x = "https://dl.irupc.xyz/405191923637340/FBI.S02E03.720p.HDTV.x265%5BSMTv%5D.mkv"; s02e04x = "https://dl.irupc.xyz/405209103506524/FBI.S02E04.720p.HDTV.x265%5BSMTv%5D.mkv"; s02e05x = "https://dl.irupc.xyz/405226283375708/FBI.S02E05.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e06x = "https://dl.irupc.xyz/405243463244892/FBI.S02E06.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e07x = "https://dl.irupc.xyz/405260643114076/FBI.S02E07.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e08x = "https://dl.irupc.xyz/405277822983260/FBI.S02E08.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e09x = "https://dl.irupc.xyz/405295002852444/FBI.S02E09.720p.HDTVx265.%5BSMTv%5D%20.mkv"; s02e10x = "https://dl.irupc.xyz/405312182721628/FBI.S02E10.720p.x265.%5BSMTv%5D.mkv";
+s02e11x = "https://dl.irupc.xyz/405329362590812/FBI.S02E11.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e12x = "https://dl.irupc.xyz/405346542459996/FBI.S02E12.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e13x = "https://dl.irupc.xyz/405363722329180/FBI.S02E13.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e14x = "https://dl.irupc.xyz/405380902198364/FBI.S02E14.720p.HDTV.x265-%5BSMTv%5D.mkv"; s02e15x = "https://dl.irupc.xyz/405398082067548/FBI.S02E15.720p.HDTV.x265-%5BSMTv%5D.mkv"; 
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@"; s02e09y = "@@02y@@"; s02e10y = "@@02y@@";
+s02e11y = "@@02y@@"; s02e12y = "@@02y@@"; s02e13y = "@@02y@@"; s02e14y = "@@02y@@"; s02e15y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E01.720p.HDTV.x265-j.mkv"; s03e02x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E02.720p.HDTV.x265-n.mkv"; s03e03x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E03.720p.HDTV.x265-n.mkv"; s03e04x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E04.720p.HDTV.x265-i.mkv"; s03e05x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E05.720p.HDTV.x265-o.mkv"; s03e06x = "https://cloud.irupc.xyz/0:/TV.Shows/FBI/S03/www.irupc.net__FBI.S03E06.720p.WEB.x265-op.mkv";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@";
+
+//Season 8 URL
+s08e16x = "https://dl.irupc.xyz/405672959974492/FBI.S02E16.480p.mkv"; s08e17x = "https://dl.irupc.xyz/405690139843676/FBI.S02E17.480p.mkv"; s08e18x = "https://dl.irupc.xyz/405707319712860/FBI.S02E18.480p.mkv"; s08e19x = "https://dl.irupc.xyz/405724499582044/FBI.S02E19.480p.mkv";
+//@@08z@@  
+s08e16y = "@@08y@@"; s08e17y = "@@08y@@"; s08e18y = "@@08y@@"; s08e19y = "@@08y@@";
