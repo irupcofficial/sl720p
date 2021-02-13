8db4788df7e29ed22ad61e52bc9b379279ccc37f
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-14 00:03:35 +0530

    Create The.Sinner_5693.js

---
+++ assets/TV/The.Sinner_5693.js
@@ -0,0 +1,20 @@
+lastRes = "720p"; lastSize = "175MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~200MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~180MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s03Res = "720p"; s03Size = "~190MB"; s03Rip = "WebRip"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/407597105323100/The.Sinner.S01E01.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e02x = "https://dl.irupc.xyz/407614285192284/The.Sinner.S01E02.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e03x = "https://dl.irupc.xyz/407631465061468/The.Sinner.S01E03.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e04x = "https://dl.irupc.xyz/407683004669020/The.Sinner.S01E04.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e05x = "https://dl.irupc.xyz/407665824799836/The.Sinner.S01E05.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e06x = "https://dl.irupc.xyz/407648644930652/The.Sinner.S01E06.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e07x = "https://dl.irupc.xyz/407700184538204/The.Sinner.S01E07.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s01e08x = "https://dl.irupc.xyz/407717364407388/The.Sinner.S01E08.720p.WEBRip.2CH.x265.HEVC_PSA.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/407871983230044/The.Sinner.S02E01.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s02e02x = "https://dl.irupc.xyz/407889163099228/The.Sinner.S02E02.REPACK.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s02e03x = "https://dl.irupc.xyz/407906342968412/The.Sinner.S02E03.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s02e04x = "https://dl.irupc.xyz/407923522837596/The.Sinner.S02E04.720p.WEBRip.2CH.x265.HEVC_PSA.mkv"; s02e05x = "https://dl.irupc.xyz/407940702706780/The.Sinner.S02E05.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s02e06x = "https://dl.irupc.xyz/407957882575964/The.Sinner.S02E06.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s02e07x = "https://dl.irupc.xyz/408112501398620/The.Sinner.S02E07.720p.WEB-DL.x265-HETeam.mkv"; s02e08x = "https://dl.irupc.xyz/407975062445148/The.Sinner.S02E08.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
+
+
+//Season 3 URL
+s03e01x = "https://dl.irupc.xyz/408146861136988/The.Sinner.S03E01.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e02x = "https://dl.irupc.xyz/408164041006172/The.Sinner.S03E02.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e03x = "https://dl.irupc.xyz/408181220875356/The.Sinner.S03E03.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e04x = "https://dl.irupc.xyz/408232760482908/The.Sinner.S03E04.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e05x = "https://dl.irupc.xyz/408198400744540/The.Sinner.S03E05.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e06x = "https://dl.irupc.xyz/408249940352092/The.Sinner.S03E06.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e07x = "https://dl.irupc.xyz/408267120221276/The.Sinner.S03E07.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv"; s03e08x = "https://dl.irupc.xyz/408215580613724/The.Sinner.S03E08.720p.WEBRip.2CH.x265.HEVC_PSA_0.mkv";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@"; s03e07y = "@@03y@@"; s03e08y = "@@03y@@";
