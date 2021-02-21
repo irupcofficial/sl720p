cdbc7ea0444818719007fb4671255acdb9a27439
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-21 08:56:28 +0530

    Create Triples_Tamil_5931.js

---
+++ assets/TV/Triples_Tamil_5931.js
@@ -0,0 +1,13 @@
+lastRes = "1080p"; lastSize = "426MB"; lastRip = "HDRip"; lastMsg1 = "x264 Codec Movie"; lastMsg2 = "---";
+s01Res = "1080p"; s01Size = "~400MB"; s01Rip = "HDRip"; s01Msg1 = "x264 Codec Movie"; s01Msg2 = "---";
+s02Res = "1080p"; s02Size = "~490MB"; s02Rip = "WebRip"; s02Msg1 = "x264 Codec Movie"; s02Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/417887846964316/%5BCL%5D%20Triples%20(2020)%20SO1%20EO1%20Tamil%201080P%20HDRip.mp4.mp4"; s01e02x = "https://dl.irupc.xyz/417905026833500/%5BCL%5D%20Triples%20(2020)%20SO1%20EO2%20Tamil%201080P%20HDRip.mp4.mp4"; s01e03x = "https://dl.irupc.xyz/417922206702684/%5BCL%5D%20Triples%20(2020)%20SO1%20EO3%20Tamil%201080P%20HDRip.mp4.mp4"; s01e04x = "https://dl.irupc.xyz/417939386571868/%5BCL%5D%20Triples%20(2020)%20SO1%20EO4%20Tamil%201080P%20HDRip.mp4.mp4"; s01e05x = "https://dl.irupc.xyz/417956566441052/%5BCL%5D%20Triples%20(2020)%20SO1%20EO5%20Tamil%201080P%20HDRip.mp4.mp4"; s01e06x = "https://dl.irupc.xyz/417973746310236/%5BCL%5D%20Triples%20(2020)%20SO1%20EO6%20Tamil%201080P%20HDRip.mp4.mp4"; s01e07x = "https://dl.irupc.xyz/417990926179420/%5BCL%5D%20Triples%20(2020)%20SO1%20EO7%20Tamil%201080P%20HDRip.mp4.mp4"; s01e08x = "https://dl.irupc.xyz/418008106048604/%5BCL%5D%20Triples%20(2020)%20SO1%20EO8%20Tamil%201080P%20HDRip.mp4.mp4";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/418162724871260/%5BCC%5D.Triples.S01E01.2020.720p.WEBRip.MultiAudio.mkv"; s02e02x = "https://dl.irupc.xyz/418179904740444/%5BCC%5D.Triples.S01E02.2020.720p.WEBRip.MultiAudio.mkv"; s02e03x = "https://dl.irupc.xyz/418197084609628/%5BCC%5D.Triples.S01E03.2020.720p.WEBRip.MultiAudio.mkv"; s02e04x = "https://dl.irupc.xyz/418214264478812/%5BCC%5D.Triples.S01E04.2020.720p.WEBRip.MultiAudio.mkv"; s02e05x = "https://dl.irupc.xyz/418231444347996/%5BCC%5D.Triples.S01E05.2020.720p.WEBRip.MultiAudio.mkv"; s02e06x = "https://dl.irupc.xyz/418248624217180/%5BCC%5D.Triples.S01E06.2020.720p.WEBRip.MultiAudio.mkv"; s02e07x = "https://dl.irupc.xyz/418265804086364/%5BCC%5D.Triples.S01E07.2020.720p.WEBRip.MultiAudio.mkv"; s02e08x = "https://dl.irupc.xyz/418282983955548/%5BCC%5D.Triples.S01E08.2020.720p.WEBRip.MultiAudio.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
