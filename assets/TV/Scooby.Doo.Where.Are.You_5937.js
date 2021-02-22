06fb6ab2099be346a324d8455b1d5511c01179c2
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-22 09:18:40 +0530

    Create Scooby.Doo.Where.Are.You_5937.js

---
+++ assets/TV/Scooby.Doo.Where.Are.You_5937.js
@@ -0,0 +1,21 @@
+lastRes = "---"; lastSize = "266MB"; lastRip = "---"; lastMsg1 = "x264 Codec Movie"; lastMsg2 = "---";
+s01Res = "---"; s01Size = "~1.7GB"; s01Rip = "---"; s01Msg1 = "x264 Codec Movie"; s01Msg2 = "---";
+s02Res = "---"; s02Size = "~837MB"; s02Rip = "---"; s02Msg1 = "x264 Codec Movie"; s02Msg2 = "---";
+s03Res = "---"; s03Size = "~260MB"; s03Rip = "---"; s03Msg1 = "x264 Codec Movie"; s03Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/423625923271772/Scooby%20Doo%20Where%20Are%20You%20(1969)%20S01%20Complete.zip";
+//@@01x@@
+s01e01y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/423660283010140/Scooby%20Doo%20Where%20Are%20You%20(1969)%20S02%20Complete.zip";
+//@@02x@@
+s02e01y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://dl.irupc.xyz/423695529735908/Episode%201%20Watch%20Out!%20The%20Willawaw.mkv"; s03e02x = "https://dl.irupc.xyz/423712709605092/Episode%202%20A%20Creepy%20Tangle%20in%20the%20Bermuda%20Triangle.mkv"; s03e03x = "https://dl.irupc.xyz/423729889474276/Episode%203%20A%20Scary%20Night%20With%20a%20Snow%20Beast%20Fright.mkv"; s03e04x = "https://dl.irupc.xyz/423747069343460/Episode%204%20To%20Switch%20a%20Witch.mkv"; s03e05x = "https://dl.irupc.xyz/423764249212644/Episode%205%20The%20Tar%20Monster.mkv"; s03e06x = "https://dl.irupc.xyz/423781429081828/Episode%206%20A%20Highland%20Fling%20With%20a%20Monstrous.mkv"; s03e07x = "https://dl.irupc.xyz/423798608951012/Episode%207%20The%20Creepy%20Case%20of%20Old%20Iron%20Face.mkv"; s03e08x = "https://dl.irupc.xyz/423815788820196/Episode%208%20Jeepers,%20It's%20the%20Jaguaro.mkv"; s03e09x = "https://dl.irupc.xyz/423832968689380/Episode%209%20Make%20a%20Beeline%20Away%20from%20That%20Feline.mkv"; s03e10x = "https://dl.irupc.xyz/423850148558564/Episode%2010%20The%20Creepy%20Creature%20of%20Vulture's%20Claw.mkv";
+s03e11x = "https://dl.irupc.xyz/423867328427748/Episode%2011%20The%20Diabolical%20Disc%20Demon.mkv"; s03e12x = "https://dl.irupc.xyz/423884508296932/Episode%2012%20Scooby's%20Chinese%20Fortune%20Kooky%20Caper.mkv"; s03e13x = "https://dl.irupc.xyz/423901688166116/Episode%2013%20A%20Menace%20In%20Venice.mkv"; s03e14x = "https://dl.irupc.xyz/423918868035300/Episode%2014%20Don't%20Go%20Near%20the%20Fortress%20of%20Fear.mkv"; s03e15x = "https://dl.irupc.xyz/423936047904484/Episode%2015%20The%20Warlock%20of%20Wimbledon.mkv"; s03e16x = "https://dl.irupc.xyz/423953227773668/Episode%2016%20The%20Beast%20Is%20Awake%20In%20Bottomless.mkv";
+//@@03y@@
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@"; s03e07y = "@@03y@@"; s03e08y = "@@03y@@"; s03e09y = "@@03y@@"; s03e10y = "@@03y@@";
+s03e11y = "@@03y@@"; s03e12y = "@@03y@@"; s03e13y = "@@03y@@"; s03e14y = "@@03y@@"; s03e15y = "@@03y@@"; s03e16y = "@@03y@@";
