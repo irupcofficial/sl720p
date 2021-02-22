17d12a4f0a8743bf62c6bb082df33a2d687911ae
Irupc <68991705+Irupc@users.noreply.github.com>
2021-02-22 18:44:34 +0530

    Create Hostages_2937.js

---
+++ assets/TV/Hostages_2937.js
@@ -0,0 +1,15 @@
+lastRes = "720p"; lastSize = "373MB"; lastRip = "WebRip"; lastMsg1 = "x264 Codec Movie"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~1.1GB"; s01Rip = "WebRip"; s01Msg1 = "x264 Codec Movie"; s01Msg2 = "---";
+s02Res = "720p"; s02Size = "~370MB"; s02Rip = "WebRip"; s02Msg1 = "x264 Codec Movie"; s02Msg2 = "---";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/424862873853020/Hostages%20S01%20E01-05%20WebRip%20720p%20-%20mkvCinemas.mkv"; s01e02x = "https://dl.irupc.xyz/424880053722204/Hostages%20S01%20E06-10%20WebRip%20720p%20-%20mkvCinemas.mkv";
+//@@01y@@
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/424450556992604/Hostages_2020_Hindi_S02E01_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e02x = "https://dl.irupc.xyz/424467736861788/Hostages_2020_Hindi_S02E02_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e03x = "https://dl.irupc.xyz/424484916730972/Hostages_2020_Hindi_S02E03_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e04x = "https://dl.irupc.xyz/424502096600156/Hostages_2020_Hindi_S02E04_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e05x = "https://dl.irupc.xyz/424519276469340/Hostages.2020.Hindi.S02E05.720p.HS.WEBRip.AAC.x264.BonsaiHD.mkv"; s02e06x = "https://dl.irupc.xyz/424536456338524/Hostages_2020_Hindi_S02E06_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e07x = "https://dl.irupc.xyz/424553636207708/Hostages_2020_Hindi_S02E07_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e08x = "https://dl.irupc.xyz/424570816076892/Hostages_2020_Hindi_S02E08_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e09x = "https://dl.irupc.xyz/424587995946076/Hostages_2020_Hindi_S02E09_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e10x = "https://dl.irupc.xyz/424622355684444/Hostages_2020_Hindi_S02E10_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv";
+s02e11x = "https://dl.irupc.xyz/424639535553628/Hostages_2020_Hindi_S02E11_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv"; s02e12x = "https://dl.irupc.xyz/424605175815260/Hostages_2020_Hindi_S02E12_720p_HS_WEBRip_AAC_ESub_x264_BonsaiHD.mkv";
+//@@02y@@
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@"; s02e09y = "@@02y@@"; s02e10y = "@@02y@@";
+s02e11y = "@@02y@@"; s02e12y = "@@02y@@";
